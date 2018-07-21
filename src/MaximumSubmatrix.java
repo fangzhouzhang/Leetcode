9ff9ecb3f2a2140853e9be517eecac0e270a1029
Fangzhou Zhang <zhangark93@gmail.com>
2018-07-22 01:17:23 +0800

    201807210117

--- src/MaximumSubmatrix.java
+++ src/MaximumSubmatrix.java
@@ -1,58 +1,60 @@
 public class MaximumSubmatrix {
+    /**
+     * @param matrix: the given matrix
+     * @return: the largest possible sum
+     * time: o(n^3) space: o(n^2)
+     */
     public int maxSubmatrix(int[][] matrix) {
         // write your code here
-        if (matrix == null || matrix.length == 0) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
-        int[][] prefix = new int[matrix.length][matrix[0].length + 1];
-        for (int i = 0; i < matrix.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < matrix[0].length; j++) {
-                sum += matrix[i][j];
-                prefix[i][j + 1] = sum;
-            }
-        }
-        /*for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length + 1; j++) {
-                System.out.println(prefix[i][j]);
-            }
-            System.out.println("****************************");
-        }*/
-        int res = 0;
-        int[] arr = new int[matrix.length];
-        for (int j = 0; j < prefix[0].length; j++) {
-            for (int k = j + 1; k < prefix[0].length; k++) {
-                for (int i = 0; i < matrix.length; i++) {
-                    arr[i] = prefix[i][k] - prefix[i][j];
-//                    System.out.println("arr[i] " + arr[i] + " prefix[i][k] " + k + " " + prefix[i][k] + " prefix[i][j] " + j + " " +  + prefix[i][j]);
+        //build prefixSum array
+        int[][] prefixSum = new int[matrix[0].length][matrix.length + 1];
+        for (int col = 0; col < matrix[0].length; col++) {
+            for (int row = 0; row < matrix.length + 1; row++) {
+                if (row == 0) {
+                    prefixSum[col][row] = 0;
+                } else {
+                    prefixSum[col][row] = matrix[row - 1][col] + prefixSum[col][row - 1];
                 }
-//                System.out.println("******************");
-                res = Math.max(res, maxSubArray(arr));
-//                System.out.println("res max is  " + res + " maxSubArray(arr) " + maxSubArray(arr));
-//                System.out.println("******************");
             }
         }
-        return res;
+        /*for (int i = 0; i < prefixSum.length; i++) {
+            for (int j = 0; j < prefixSum[0].length; j++) {
+                System.out.print(" " + prefixSum[i][j] + " ");
+            }
+            System.out.println();
+        }*/
+        int[] input = new int[matrix[0].length];
+        int global_max = Integer.MIN_VALUE;
+        for (int upper = 0; upper < matrix.length; upper++) {
+            for (int lower = upper; lower < matrix.length; lower++) {
+                for (int i = 0; i < matrix[0].length; i++) {
+                    input[i] = prefixSum[i][lower + 1] - prefixSum[i][upper];
+                }
+                global_max = Math.max(maxSubArray(input), global_max);
+            }
+        }
+        return global_max;
     }
-
-
     public int maxSubArray(int[] nums) {
         // write your code here
-        int[] prefix = new int[nums.length + 1];
-        prefix[0] = 0;
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            prefix[i + 1] = sum;
+        //time: o(n)
+        if (nums.length == 0) {
+            return -1;
         }
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < prefix.length; i++) {
-            for (int j = i + 1; j < prefix.length; j++) {
-                if (prefix[j] - prefix[i] > max) {
-                    max = prefix[j] - prefix[i];
-                }
+        int[] m = new int[nums.length];
+        m[0] = nums[0];
+        int global_max = Math.max(Integer.MIN_VALUE, m[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (m[i - 1] < 0) {
+                m[i] = nums[i];
+            } else {
+                m[i] = nums[i] + m[i - 1];
             }
+            global_max = Math.max(global_max, m[i]);
         }
-        return max;
+        return global_max;
     }
 }
