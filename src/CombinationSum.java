374f65e48c3aead6220e968c0015420be4c9b3ed
fz7 <fz7@illinois.edu>
2018-08-19 13:49:39 -0700

    201808191349

--- src/CombinationSum.java
+++ src/CombinationSum.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CombinationSum {
     /**
@@ -13,28 +11,33 @@ public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         // write your code here
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        int[] count = new int[candidates.length];
         Arrays.sort(candidates);
-        dfs(res, count, candidates, target, 0);
+        dfs(res, new ArrayList<Integer>(), candidates, target, 0, new HashSet<Integer>());
         return res;
     }
-    public void dfs(List<List<Integer>> res, int[] count, int[] candidates, int target, int level) {
+    public void dfs(List<List<Integer>> res, List<Integer> temp, int[] candidates, int target, int level, Set<Integer> set) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
         if (level == candidates.length) {
-            if (target == 0) {
-                List<Integer> temp = new ArrayList<>();
-                for (int i = 0; i < count.length; i++) {
-                    for (int j = 0; j < count[i]; j++) {
-                        temp.add(candidates[i]);
-
-                    }
+            return;
+        }
+        if (set.contains(candidates[level])) {
+            dfs(res, temp, candidates, target, level + 1, set);
+        } else {
+            set.add(candidates[level]);
+            for (int i = 0; i * candidates[level] <= target; i++) {
+                for (int j = 1; j <= i; j++) {
+                    temp.add(candidates[level]);
                 }
-                res.add(temp);
+                dfs(res, temp, candidates, target - candidates[level] * i, level + 1, set);
+                for (int j = 1; j <= i; j++) {
+                    temp.remove(temp.size() - 1);
+                }
             }
-            return;//base case
+            set.remove(candidates[level]);
         }
-        for (int i = 0; i <= target / candidates[level]; i++) {
-            count[level] = i;
-            dfs(res, count, candidates, target - candidates[level] * i, level + 1);
-        }
+
     }
 }
