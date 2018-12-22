a5ce3cf6283dd2db037293ab96f6beff4ab19f9b
fangzhouzhang <zhangark93@gmail.com>
2018-12-22 01:31:19 -0800

    201812220131

---
+++ src/SerializeandDeserializeBinaryTree.java
@@ -0,0 +1,35 @@
+public class SerializeandDeserializeBinaryTree {
+	// Encodes a tree to a single string.
+	public String serialize(TreeNode root) {
+		StringBuilder res = new StringBuilder();
+		strHelper(root, res);
+		res.deleteCharAt(res.length() - 1);
+		return new String(res);
+	}
+	private void strHelper(TreeNode root, StringBuilder res) {
+		if (root == null) {
+			res.append("null,");
+			return;
+		}
+		res.append(root.val + ",");
+		strHelper(root.left, res);
+		strHelper(root.right, res);
+	}
+	// Decodes your encoded data to tree.
+	public TreeNode deserialize(String data) {
+		String[] arr = data.split(",");
+		int[] level = new int[1];
+		return constructTree(arr, level);
+	}
+	private TreeNode constructTree(String[] arr, int[] level) {
+		if (level[0] == arr.length || arr[level[0]].equals("null")) {
+			return null;
+		}
+		TreeNode root = new TreeNode(Integer.valueOf(arr[level[0]]));
+		level[0]++;
+		root.left = constructTree(arr, level);
+		level[0]++;
+		root.right = constructTree(arr, level);
+		return root;
+	}
+}
