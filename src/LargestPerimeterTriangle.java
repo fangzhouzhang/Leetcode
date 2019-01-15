5a6dd71e211afcddc5ebe53a159c5402e1ea5382
fangzhouzhang <zhangark93@gmail.com>
2019-01-14 23:41:56 -0800

    201901142341

---
+++ src/LargestPerimeterTriangle.java
@@ -0,0 +1,32 @@
+public class LargestPerimeterTriangle {
+	public int largestPerimeter(int[] A) {
+		if (A == null || A.length < 3) {
+			return 0;
+		}
+		int max = 0;
+		Arrays.sort(A);
+		for (int i = A.length - 1; i >= 2; i--) {
+			for (int j = i - 1; j >= 1; j--) {
+				for (int k = j - 1; k >= 0; k--) {
+					if (isValid(A, i, j, k)) {
+						// System.out.println(A[i] + " " + A[j] + " " +  A[k]);
+						max = Math.max(max, A[i] + A[j] + A[k]);
+						return max;
+					}
+				}
+			}
+		}
+		return max;
+	}
+	private boolean isValid(int[] a, int i, int j, int k) {
+		if (a[i] + a[j] <= a[k] || a[i] + a[k] <= a[j] || a[j] + a[k] <= a[i]) {
+			return false;
+		}
+		if (Math.abs(a[i] - a[j]) >= a[k] ||
+				Math.abs(a[i] - a[k]) >= a[j] ||
+				Math.abs(a[j] - a[k]) >= a[i]) {
+			return false;
+		}
+		return true;
+	}
+}
