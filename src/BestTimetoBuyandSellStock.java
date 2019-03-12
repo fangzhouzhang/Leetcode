202cbd788c49866ae5a8ed330d24993b2c704635
fangzhouzhang <zhangark93@gmail.com>
2019-03-11 23:04:15 -0700

    201903112304

--- src/BestTimetoBuyandSellStock.java
+++ src/BestTimetoBuyandSellStock.java
@@ -1,22 +1,19 @@
 public class BestTimetoBuyandSellStock {
 	/**
-	 * time: o(n^2) space: o(n)
+	 * time: o(n) space: o(1)
 	 * @param prices
 	 * @return
 	 */
 	public int maxProfit(int[] prices) {
-		if (prices.length == 1) {
-			return 0;
-		}
-		int[] dp = new int[prices.length];
-		int max = 0;
-		for (int i = prices.length - 1; i >= 1; i--) {
-			for (int j = i - 1; j >= 0; j--) {
-				if (prices[i] - prices[j] > 0) {
-					max = Math.max(max, prices[i] - prices[j]);
-				}
+		int maxProfit = 0;
+		int minPrice = Integer.MAX_VALUE;
+		for (int i = 0; i < prices.length; i++) {
+			if (prices[i] < minPrice) {
+				minPrice = prices[i];
+			} else if (prices[i] - minPrice > maxProfit) {
+				maxProfit = prices[i] - minPrice;
 			}
 		}
-		return max;
+		return maxProfit;
 	}
 }
