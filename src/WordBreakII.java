a2917062e165552c12f217b9087431c3b31e5f27
fangzhouzhang <zhangark93@gmail.com>
2019-05-19 06:07:47 -0700

    201905190607

--- src/WordBreakII.java
+++ src/WordBreakII.java
@@ -6,44 +6,43 @@ import java.util.Set;
 public class WordBreakII {
 	public List<String> wordBreak(String s, List<String> wordDict) {
 		List<String> res = new ArrayList<>();
-		if (s == null || s.length() == 0) {
-			res.add("");
-			return res;
-		}
 		Set<String> set = new HashSet<>(wordDict);
-		dfs(s, set, res, 0, "", new boolean[s.length()]);
+		StringBuilder sb = new StringBuilder();
+
+		Boolean[] success = new Boolean[s.length()];//success[i] means from i to len - 1, whether we can find solution
+		dfs(res, s, set, "", success, 0);
 		return res;
 	}
-	private void dfs(String s,
-					 Set<String> set,
-					 List<String> res,
-					 int level,
-					 String temp,
-					 boolean[] fail) {
-		if (level == s.length()) {
-			res.add(new String(temp));
+	private void dfs(List<String> res, String s, Set<String> set, String sb, Boolean[] success, int idx) {
+		if (idx == s.length()) {
+			res.add(new String(sb));
 			return;
 		}
-		int curSize = res.size();
-		for (int i = 1; level + i <= s.length(); i++) {
-			if (isValid(s, level, level + i, set) && fail[level] == false) {
-				if (level == 0) {
-					dfs(s, set, res, level + i, temp + s.substring(level, level + i), fail);
-				} else {
-					dfs(s, set, res, level + i, temp + " " + s.substring(level, level + i), fail);
+		// idx < len
+		if (success[idx] != null && !success[idx]) {
+			return; //fail
+		}
 
+		int size = sb.length();
+		int prev = res.size();
+
+		for (int i = idx; i < s.length(); i++) {
+			String cut = s.substring(idx, i + 1);
+			if (set.contains(cut)) {
+				if (size == 0) {
+					dfs(res, s, set, sb + cut, success, i + 1);
+				} else {
+					dfs(res, s, set, sb + " " + cut, success, i + 1);
 				}
 			}
 		}
-		if (curSize == res.size()) {
-			fail[level] = true;
+
+		if (res.size() != prev) {
+			success[idx] = true;
+		} else {
+			success[idx] = false;
 		}
-	}
-	private boolean isValid(String s, int start, int end, Set<String> set) {
-		String check = s.substring(start, end);
-		if (set.contains(check)) {
-			return true;
-		}
-		return false;
+		//set back
+		//sb.setLength(size);
 	}
 }
