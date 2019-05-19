45301052e56a38e9747e20468ea9527ec9c9da75
fangzhouzhang <zhangark93@gmail.com>
2019-05-19 01:07:16 -0700

    201905190107

--- src/RegularExpressionMatching.java
+++ src/RegularExpressionMatching.java
@@ -28,4 +28,43 @@ public class RegularExpressionMatching {
 		}
 		return dp[s.length()][p.length()];
 	}
+	public boolean isMatch2(String s, String p) {
+		Boolean[][] dp = new Boolean[s.length() + 1][p.length() + 1];
+		dfs(s, 0, p, 0, dp);
+		return dp[0][0];
+	}
+	private boolean dfs(String s, int idxs, String p, int idxp, Boolean[][] dp) {
+		if (idxp == p.length()) {
+			boolean val = idxs == s.length();
+			dp[idxs][idxp] = val;
+			return val;
+		}
+		if (dp[idxs][idxp] != null) {
+			return dp[idxs][idxp];
+		}
+		if (idxp == p.length() - 1 || p.charAt(idxp + 1) != '*') {
+			//letter or '.'
+			if (idxs < s.length() && (p.charAt(idxp) == '.' || p.charAt(idxp) == s.charAt(idxs))) {
+				boolean val = dfs(s, idxs + 1, p, idxp + 1, dp);
+				dp[idxs][idxp] = val;
+				return val;
+			} else {
+				boolean val = false;
+				dp[idxs][idxp] = val;
+				return val;
+			}
+		} else {
+			int i = idxs - 1;
+			while (i < s.length() && (i == idxs - 1 || p.charAt(idxp) == '.' || p.charAt(idxp) == s.charAt(i))) {
+				if (dfs(s, i + 1, p, idxp + 2, dp)) {
+					dp[idxs][idxp] = true;
+					return true;
+				} else {
+					i++;
+				}
+			}
+			dp[idxs][idxp] = false;
+			return false;
+		}
+	}
 }
