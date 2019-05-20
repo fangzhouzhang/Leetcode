6735f6b5a33d8290e06cf103819cb459b862e13b
fangzhouzhang <zhangark93@gmail.com>
2019-05-19 21:39:22 -0700

    201905192139

--- src/CanIWin.java
+++ src/CanIWin.java
@@ -10,15 +10,18 @@ public class CanIWin {
 		if ((1 + maxChoosableInteger) * maxChoosableInteger / 2 < desiredTotal) {
 			return false;
 		}
-		Map<String, Boolean> cache = new HashMap<>();
+		// Map<String, Boolean> cache = new HashMap<>();
+		Map<Integer, Boolean> cache = new HashMap<>();
 		boolean[] used = new boolean[maxChoosableInteger + 1];
 		return dfs(used, desiredTotal, 0, cache);
 	}
-	private boolean dfs(boolean[] used, int desiredTotal, int curSum, Map<String, Boolean> cache) {
+	// private boolean dfs(boolean[] used, int desiredTotal, int curSum, Map<String, Boolean> cache) {
+	private boolean dfs(boolean[] used, int desiredTotal, int curSum, Map<Integer, Boolean> cache) {
 		if (curSum >= desiredTotal) {
 			return false;
 		}
-		String key = Arrays.toString(used);
+		// String key = Arrays.toString(used);
+		Integer key = format(used);
 		Boolean res = cache.get(key);
 		if (res != null) {
 			return res;
@@ -36,5 +39,13 @@ public class CanIWin {
 		}
 		cache.put(key, false);
 		return false;
+	}
+	private int format(boolean[] used){
+		int num = 0;
+		for(boolean b: used){
+			num <<= 1;
+			if(b) num |= 1;
+		}
+		return num;
 	}
 }
