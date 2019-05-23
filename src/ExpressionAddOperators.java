73feec3d195bf0ced905f7ae43324de05d9bf361
fangzhouzhang <zhangark93@gmail.com>
2019-05-23 08:07:06 -0700

    201905230806

--- src/ExpressionAddOperators.java
+++ src/ExpressionAddOperators.java
@@ -7,8 +7,58 @@ public class ExpressionAddOperators {
 		if (num == null || num.length() == 0) {
 			return res;
 		}
-		dfs(res, new StringBuilder(), num, target, 0, 0, 0);
+		char[] sc = num.toCharArray();
+		char[] path = new char[sc.length * 2];
+		// dfs(res, new StringBuilder(), num, target, 0, 0, 0);
+		dfs1(res, sc, path, target, 0, 0, 0, 0);
 		return res;
+	}
+	private void dfs1(List<String> res, char[] sc, char[] path, int target, long sum, long lastNum, int idxs, int idxp) {
+		if (idxs == sc.length && sum == target) {
+			// System.out.println("target " + target + " sum " + sum + " last num " +  lastNum);
+			res.add(new String(path, 0, idxp));
+			return;
+		}
+		if (idxs == sc.length) {
+			return;
+		}
+		long val = 0;
+		int signIdx = idxp;
+		if (idxs == 0) {
+			for (int i = idxs; i < sc.length && idxp < path.length; i++, idxp++) {
+				val = val * 10 + sc[i] - '0';
+				path[idxp] = sc[i];
+				dfs1(res, sc, path, target, sum + val, val, i + 1, idxp + 1);
+				if (val == 0) {
+					break;
+				}
+			}
+		} else {
+			idxp++;
+			for (int i = idxs; i < sc.length && idxp < path.length; i++, idxp++) {
+				val = val * 10 + sc[i] - '0';
+				//+
+				path[signIdx] = '+';
+				path[idxp] = sc[i];
+				dfs1(res, sc, path, target, sum + val, val, i + 1, idxp + 1);
+
+				//-
+				path[signIdx] = '-';
+				path[idxp] = sc[i];
+				dfs1(res, sc, path, target, sum - val, -val, i + 1, idxp + 1);
+
+				//*
+				//+
+				path[signIdx] = '*';
+				path[idxp] = sc[i];
+				dfs1(res, sc, path, target, sum - lastNum + lastNum * val, lastNum * val, i + 1, idxp + 1);
+
+				if (val == 0) {
+					break;
+				}
+			}
+		}
+
 	}
 	private void dfs(List<String> res,
 					 StringBuilder sb,
@@ -16,70 +66,45 @@ public class ExpressionAddOperators {
 					 int target,
 					 long sum,
 					 long lastNum,
-					 int level) {
-		if (sum == (long)target && level == num.length()) {
+					 int idx) {
+		//success
+		if (idx == num.length() && sum == target) {
+			// System.out.println("target " + target + " sum " + sum + " last num " +  lastNum);
 			res.add(new String(sb));
 			return;
 		}
-		if (sum != (long)target && level >= num.length()) {
+		//fail
+		if (idx == num.length()) {
 			return;
 		}
-		if (level == 0) {
-			int prevLen  = sb.length();
-			long cur = 0;
-			for (int offset = 1; level + offset <= num.length(); offset++) {
-				if (offset != 1 && num.charAt(level) == '0') {
-					continue;
-				}
-				cur = Long.parseLong(num.substring(level, level + offset));
-				sb.append(cur);
-				dfs(res, sb, num, target, sum + cur, cur, level + offset);
+		int prevLen = sb.length();
+
+		for (int i = idx; i < num.length(); i++) {
+			Long cut = Long.parseLong(num.substring(idx, i + 1));
+
+			if (prevLen != 0) {
+				//+
+				sb.append("+" + cut);
+				dfs(res, sb, num, target, sum + cut, cut, i + 1);
+				sb.setLength(prevLen);
+				//-
+				sb.append("-" + cut);
+				dfs(res, sb, num, target, sum - cut, -cut, i + 1);
+				sb.setLength(prevLen);
+				//*
+				sb.append("*" + cut);
+				dfs(res, sb, num, target, sum - lastNum + lastNum * cut, lastNum * cut, i + 1);
+				sb.setLength(prevLen);
+			} else if (prevLen == 0) {
+				sb.append(cut);
+				dfs(res, sb, num, target, sum + cut, cut, i + 1);
 				sb.setLength(prevLen);
 			}
-		} else {//level >= 1
-			for (int i = 0; i < 3; i++) {
-				if (i == 0) {//+
-					int prevLen  = sb.length();
-					long cur = 0;
-					for (int offset = 1; level + offset <= num.length(); offset++) {
-						if (offset != 1 && num.charAt(level) == '0') {
-							continue;
-						}
-						cur = Long.parseLong(num.substring(level, level + offset));
-						sb.append("+");
-						sb.append(cur);
-						dfs(res, sb, num, target, sum + cur, cur, level + offset);
-						sb.setLength(prevLen);
-					}
-				} else if (i == 1) {//-
-					int prevLen  = sb.length();
-					long cur = 0;
-					for (int offset = 1; level + offset <= num.length(); offset++) {
-						if (offset != 1 && num.charAt(level) == '0') {
-							continue;
-						}
-						cur = Long.parseLong(num.substring(level, level + offset));
-						sb.append("-");
-						sb.append(cur);
-						dfs(res, sb, num, target, sum + (-1 * cur), -1 * cur, level + offset);
-						sb.setLength(prevLen);
-					}
-				} else {//*
-					int prevLen  = sb.length();
-					long cur = 0;
-					for (int offset = 1; level + offset <= num.length(); offset++) {
-						if (offset != 1 && num.charAt(level) == '0') {
-							continue;
-						}
-						cur = Long.parseLong(num.substring(level, level + offset));
-						sb.append("*");
-						sb.append(cur);
-						dfs(res, sb, num, target, sum - lastNum + lastNum * cur, lastNum * cur, level + offset);
-						sb.setLength(prevLen);
-					}
-				}
+			if (cut == 0) {
+				break;
 			}
 		}
+
 
 	}
 }
