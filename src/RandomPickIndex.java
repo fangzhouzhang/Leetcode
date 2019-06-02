2a21c26ef5b4b23d3a4ea2fd57477cf62c401298
fangzhouzhang <zhangark93@gmail.com>
2019-06-02 00:07:49 -0700

    201906020007

--- src/RandomPickIndex.java
+++ src/RandomPickIndex.java
@@ -1,32 +1,25 @@
 import java.util.*;
 
 public class RandomPickIndex {
-	private Map<Integer, List<Integer>> map;
 	private Random r;
+	private int[] a;
 	public RandomPickIndex(int[] nums) {
 		r = new Random();
-		map = new HashMap<>();
-		buildMap(map, nums);
+		a = nums;
 	}
 
 	public int pick(int target) {
-		//it must exist
-		List<Integer> list = map.get(target);
-		int size = list.size();
-		int idx = r.nextInt(size);
-		return list.get(idx);
-	}
-	private void buildMap(Map<Integer, List<Integer>> map, int[] nums) {
-		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(nums[i])) {
-				List<Integer> list = map.get(nums[i]);
-				list.add(i);
-				map.put(nums[i], list);
-			} else {
-				List<Integer> list = new ArrayList<>();
-				list.add(i);
-				map.put(nums[i], list);
+		//pick any 1 from target list
+		int res = 0;
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == target) {
+				int index = r.nextInt(++count);
+				if (index == 0) {
+					res = i;
+				}
 			}
 		}
+		return res;
 	}
 }
