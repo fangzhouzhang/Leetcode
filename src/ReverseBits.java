d032ff408c8e6520da85946de6ebcaa19040b078
fangzhouzhang <zhangark93@gmail.com>
2019-08-26 08:40:11 -0700

    201908260840

--- src/ReverseBits.java
+++ src/ReverseBits.java
@@ -13,4 +13,13 @@ public class ReverseBits {
 		}
 		return n;
 	}
+	public int reverseBits1(int n) {
+		int ret = n;
+		ret = (ret & 0xffff0000) >>> 16 | (ret & 0x0000ffff) << 16;
+		ret = (ret & 0xff00ff00) >>> 8 | (ret & 0x00ff00ff) << 8;
+		ret = (ret & 0xf0f0f0f0) >>> 4 | (ret & 0x0f0f0f0f) << 4;
+		ret = (ret & 0xcccccccc) >>> 2 | (ret & 0x33333333) << 2;
+		ret = (ret & 0xaaaaaaaa) >>> 1 | (ret & 0x55555555) << 1;
+		return ret;
+	}
 }
