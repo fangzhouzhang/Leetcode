7eb6ee502a2550b15dbe7e0df42089cd69f12066
fangzhouzhang <zhangark93@gmail.com>
2019-09-01 08:42:09 -0700

    201909010841

---
+++ src/MeetingRooms.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MeetingRooms {
+	public boolean canAttendMeetings(int[][] intervals) {
+		Arrays.sort(intervals, new StartComparator());
+		List<Integer> start = new ArrayList<>();
+		for (int[] i : intervals) {
+			start.add(i[0]);
+		}
+		Arrays.sort(intervals, new EndComparator());
+		List<Integer> end = new ArrayList<>();
+		for (int[] i : intervals) {
+			end.add(i[1]);
+		}
+		int startIdx = 0, endIdx = 0;
+		int rooms = 0;
+		while (startIdx < intervals.length && endIdx < intervals.length) {
+			if (start.get(startIdx) < end.get(endIdx)) {
+				rooms++;
+				startIdx++;
+			} else {
+				rooms--;
+				endIdx++;
+			}
+			if (rooms > 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private class StartComparator implements Comparator<int[]> {
+		public int compare(int[] i1, int[] i2) {
+			return i1[0] - i2[0];
+		}
+	}
+
+	private class EndComparator implements Comparator<int[]> {
+		public int compare(int[] i1, int[] i2) {
+			return i1[1] - i2[1];
+		}
+	}
+}
