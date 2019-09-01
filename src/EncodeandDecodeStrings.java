051a71208b8e8bc3b57fe20bfec9f5acc7e782dc
fangzhouzhang <zhangark93@gmail.com>
2019-09-01 12:02:11 -0700

    201909011202

---
+++ src/EncodeandDecodeStrings.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeandDecodeStrings {
+	// Encodes a list of strings to a single string.
+	public String encode(List<String> strs) {
+		StringBuilder sb = new StringBuilder();
+		for (String str: strs) {
+			sb.append(str.length()).append("/").append(str);
+		}
+		return new String(sb);
+	}
+
+	// Decodes a single string to a list of strings.
+	public List<String> decode(String s) {
+		List<String> ret = new ArrayList<>();
+		int idx = 0;
+		while (idx < s.length()) {
+			int slash = s.indexOf("/", idx);
+			int size = Integer.valueOf(s.substring(idx, slash));
+			idx = slash + size + 1;
+			ret.add(s.substring(slash + 1, idx));
+		}
+		return ret;
+	}
+}
