f537132493fc89eff7fe0c4181fc7003a627fc96
fangzhouzhang <zhangark93@gmail.com>
2019-09-02 12:50:41 -0700

    201909021250

---
+++ src/WiggleSort.java
@@ -0,0 +1,18 @@
+public class WiggleSort {
+	public void wiggleSort(int[] nums) {
+		if (nums.length <= 1) {
+			return;
+		}
+		for (int i = 1; i < nums.length; i++) {
+			if (((i % 2 == 1) && (nums[i] < nums[i - 1])) || ((i % 2 == 0) && (nums[i] > nums[i - 1]))) {
+				swap(nums, i, i - 1);
+			}
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
+}
