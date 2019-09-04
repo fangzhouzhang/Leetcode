d4669301919d960b206fdea2f12594abdacaa0d7
fangzhouzhang <zhangark93@gmail.com>
2019-09-03 22:47:10 -0700

    201909032247

---
+++ src/GuessNumberHigherorLower.java
@@ -0,0 +1,21 @@
+public class GuessNumberHigherorLower {
+	public int guessNumber(int n) {
+		int start = 1;
+		int end = n;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (guess(mid) == -1) {
+				end = mid - 1;
+			} else if (guess(mid) == 1) {
+				start = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	private int guess(int num) {
+		return 0;
+	}
+}
