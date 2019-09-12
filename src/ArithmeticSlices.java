2c1372f41d918c4d9d42e3a39fa4f60488fb12f7
fangzhouzhang <zhangark93@gmail.com>
2019-09-12 14:17:56 -0700

    201909121417

---
+++ src/ArithmeticSlices.java
@@ -0,0 +1,18 @@
+public class ArithmeticSlices {
+	public int numberOfArithmeticSlices(int[] A) {
+		if (A.length <= 2) {
+			return 0;
+		}
+		int count = 0;
+		int sum = 0;
+		for (int i = 2; i < A.length; i++) {
+			if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+				count++;
+			} else {
+				sum += count * (count + 1) / 2;
+				count = 0;
+			}
+		}
+		return sum += count * (count + 1) / 2;
+	}
+}
