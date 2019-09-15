dcf0e8d253ea3b5ff70c66faeae1c3563ff5cea2
fangzhouzhang <zhangark93@gmail.com>
2019-09-15 14:01:38 -0700

    201909151401

---
+++ src/FindAllNumbersDisappearedinanArray.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedinanArray {
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		if (nums.length == 0) {
+			return res;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			int idx = Math.abs(nums[i]) - 1;
+			nums[idx] = (nums[idx] > 0) ? -nums[idx] : nums[idx];
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				res.add(i + 1);
+			}
+		}
+		return res;
+	}
+}
