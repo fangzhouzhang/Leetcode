c84df9078a56bf8c5209f899d64f97169aebf1d2
fangzhouzhang <zhangark93@gmail.com>
2019-09-16 11:14:29 -0700

    201909161114

---
+++ src/IslandPerimeter.java
@@ -0,0 +1,23 @@
+public class IslandPerimeter {
+	public int islandPerimeter(int[][] grid) {
+		if (grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
+		int res = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == 0) {
+					continue;
+				}
+				res += 4;
+				if (i > 0 && grid[i - 1][j] == 1) {
+					res -= 2;
+				}
+				if (j > 0 && grid[i][j - 1] == 1) {
+					res -= 2;
+				}
+			}
+		}
+		return res;
+	}
+}
