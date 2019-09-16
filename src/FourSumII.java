7db0f5073327e81a0bd43126a87cf4e2990f6a65
fangzhouzhang <zhangark93@gmail.com>
2019-09-15 18:38:13 -0700

    201909151838

---
+++ src/FourSumII.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class FourSumII {
+	public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < B.length; j++) {
+				map.put(A[i] + B[j], map.getOrDefault(A[i] + B[j], 0) + 1);
+			}
+		}
+		int res = 0;
+		for (int i = 0; i < C.length; i++) {
+			for (int j = 0; j < D.length; j++) {
+				if (map.containsKey(-(C[i] + D[j])))
+					res += map.get(-(C[i] + D[j]));
+			}
+		}
+		return res;
+	}
+}
