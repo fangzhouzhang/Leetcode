40f3f92907cac82d07a674ee73ae26a866c65077
fangzhouzhang <zhangark93@gmail.com>
2019-09-21 11:42:20 -0700

    201909211142

---
+++ src/ArrayPartitionI.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArrayPartitionI {
+	public int arrayPairSum(int[] nums) {
+		if (nums.length == 0) return 0;
+		Arrays.sort(nums);
+		int res = 0;
+		for (int i = 0; i < nums.length; i += 2) {
+			res += nums[i];
+		}
+		return res;
+	}
+}
