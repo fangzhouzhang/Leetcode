61460dbecf87ded1b0cde284ce186dcb3237cf4d
fangzhouzhang <zhangark93@gmail.com>
2019-09-21 13:58:34 -0700

    201909211358

---
+++ src/SubtreeofAnotherTree.java
@@ -0,0 +1,15 @@
+public class SubtreeofAnotherTree {
+	public boolean isSubtree(TreeNode s, TreeNode t) {
+		if (s == null && t == null) return true;
+		if (s == null || t == null ) return false;
+		if (isSameTree(s, t)) return true;
+		return isSubtree(s.left, t) || isSubtree(s.right, t);
+	}
+
+	private boolean isSameTree(TreeNode s, TreeNode t) {
+		if (s == null && t == null) return true;
+		if (s == null || t == null ) return false;
+		if (s.val != t.val) return false;
+		return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+	}
+}
