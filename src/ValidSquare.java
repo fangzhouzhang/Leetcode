50a73a9ac5328f6e21b8b0f48321a0c9d3f5ff4f
fangzhouzhang <zhangark93@gmail.com>
2019-09-21 21:56:38 -0700

    201909212156

---
+++ src/ValidSquare.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSquare {
+	public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+		int[][] a = new int[][]{p1, p2, p3, p4};
+		Set<Long> set = new HashSet<>();
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				long dis = (long)((a[i][0] - a[j][0]) * (a[i][0] - a[j][0]) + (a[i][1] - a[j][1]) * (a[i][1] - a[j][1]));
+				if (dis == 0) return false;
+				set.add(dis);
+			}
+		}
+		return set.size() == 2;
+	}
+}
