ec9be1d4c2918156d47506691c2bcdf591dd066a
fangzhouzhang <zhangark93@gmail.com>
2019-09-27 14:12:56 -0700

    201909271412

---
+++ src/MinimumDominoRotationsForEqualRow.java
@@ -0,0 +1,19 @@
+public class MinimumDominoRotationsForEqualRow {
+	public int minDominoRotations(int[] A, int[] B) {
+		int a = Math.min(getMin(A[0], A, B), getMin(A[0], B, A));
+		int b = Math.min(getMin(B[0], A, B), getMin(B[0], B, A));
+		int res = Math.min(a, b);
+		return res == Integer.MAX_VALUE ? -1 : res;
+	}
+
+	private int getMin(int target, int[] a, int[] b) {
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != target) {
+				if (b[i] == target) count++;
+				else return Integer.MAX_VALUE;
+			}
+		}
+		return count;
+	}
+}
