462723b5cac1214aeb4061832652adf7f5d0a323
fangzhouzhang <zhangark93@gmail.com>
2019-10-13 22:50:28 -0700

    201910132250

--- src/WildcardMatching.java
+++ src/WildcardMatching.java
@@ -1,51 +1,28 @@
 public class WildcardMatching {
 	public boolean isMatch(String s, String p) {
-		if (s.length() == 0 && p.length() == 0 || s.equals(p)) {
-			return true;
-		}
-		Boolean[][] mem = new Boolean[s.length()][p.length()];
-		return dfs(s, 0, p, 0, mem);
-	}
-	private boolean dfs(String s, int idxs, String p, int idxp, Boolean[][] mem) {
-		if (idxp == p.length()) {
-			return idxs == s.length();
-		}
-
-		if (idxs == s.length()) {
-			while (idxp < p.length()) {
-				if (p.charAt(idxp) != '*') {
-					return false;
-				}
-				idxp++;
-			}
-			return true;
-		}
-
-
-		if (idxs < s.length() && mem[idxs][idxp] != null) {
-			return mem[idxs][idxp];
-		}
-
-
-		char ch = p.charAt(idxp);
-		if (ch != '*') {
-			//char or '?'
-			if (idxs < s.length() && (ch == '?' || ch == s.charAt(idxs))) {
-				return dfs(s, idxs + 1, p, idxp + 1, mem);
+		int sIdx = 0;
+		int pIdx = 0;
+		int sTmp = 0;
+		int starIdx = -1;
+		while (sIdx < s.length()) {
+			if (pIdx < p.length() && (p.charAt(pIdx) == s.charAt(sIdx) || p.charAt(pIdx) == '?')) {
+				sIdx++;
+				pIdx++;
+			} else if (pIdx < p.length() && p.charAt(pIdx) == '*') {
+				starIdx = pIdx;
+				sTmp = sIdx;
+				pIdx++;
+			} else if (starIdx != -1) {
+				sIdx = sTmp + 1;
+				sTmp = sIdx;
+				pIdx = starIdx + 1;
 			} else {
-				mem[idxs][idxp] = false;
 				return false;
 			}
-		} else {
-			//cur char is '*'
-			for (int i = idxs - 1; i < s.length(); i++) {
-				if (dfs(s, i + 1, p, idxp + 1, mem)) {
-					mem[idxs][idxp] = true;
-					return true;
-				}
-			}
-			mem[idxs][idxp] = false;
-			return false;
 		}
+		while (pIdx < p.length() && p.charAt(pIdx) == '*') {
+			pIdx++;
+		}
+		return pIdx == p.length();
 	}
 }
