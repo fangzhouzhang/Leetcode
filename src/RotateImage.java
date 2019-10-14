5c91d03556f1a9ecb73dab8fad24a69216306c29
fangzhouzhang <zhangark93@gmail.com>
2019-10-14 15:57:11 -0700

    201910141557

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -1,18 +1,18 @@
 public class RotateImage {
 	public void rotate(int[][] matrix) {
-		if (matrix.length == 0 || matrix[0].length == 0) {
-			return;
-		}
-		for (int i = 0; i < matrix.length / 2; i++) {
-			int[] tmp = matrix[i];
-			matrix[i] = matrix[matrix.length - 1 - i];
-			matrix[matrix.length - 1 - i] = tmp;
-		}
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = i + 1; j < matrix[0].length; j++) {
+		int n = matrix.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
 				int tmp = matrix[i][j];
 				matrix[i][j] = matrix[j][i];
 				matrix[j][i] = tmp;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n / 2; j++) {
+				int tmp = matrix[i][j];
+				matrix[i][j] = matrix[i][n - 1 - j];
+				matrix[i][n - 1 - j] = tmp;
 			}
 		}
 	}
