4f19bbb733038f49c3a49bf49846304c09d98e18
fangzhouzhang <zhangark93@gmail.com>
2019-10-15 00:58:34 -0700

    201910150058

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -11,35 +11,26 @@ public class SpiralMatrix {
 		int left = 0;
 		int bottom = matrix.length - 1;
 		int right = matrix[0].length - 1;
-		while (true) {
-			for (int i = left; i <= right; i++) {
-				res.add(matrix[top][i]);
+		int dir = 0;
+		while (top <= bottom && left <= right) {
+			if (dir == 0) {
+				for (int i = left; i <= right; i++) res.add(matrix[top][i]);
+				top++;
+				dir = 1;
+			} else if (dir == 1) {
+				for (int i = top; i <= bottom; i++) res.add(matrix[i][right]);
+				right--;
+				dir = 2;
+			} else if (dir == 2) {
+				for (int i = right; i >= left; i--) res.add(matrix[bottom][i]);
+				bottom--;
+				dir = 3;
+			} else {
+				for (int i = bottom; i >= top; i--) res.add(matrix[i][left]);
+				left++;
+				dir = 0;
 			}
-			if (++top > bottom) {
-				break;
-			}
-			for (int i = top; i <= bottom; i++) {
-				res.add(matrix[i][right]);
-			}
-			if (--right < left) {
-				break;
-			}
-			for (int i = right; i >= left; i--) {
-				res.add(matrix[bottom][i]);
-			}
-			if (--bottom < top) {
-				break;
-			}
-			for (int i = bottom; i >= top; i--) {
-				res.add(matrix[i][left]);
-			}
-			if (++left > right) {
-				break;
-			}
-
-
 		}
-
 		return res;
 	}
 }
