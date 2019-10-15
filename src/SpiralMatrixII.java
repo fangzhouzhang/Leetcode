d3e7d20a2829b2ed9793a398cd20fc17ee0aee08
fangzhouzhang <zhangark93@gmail.com>
2019-10-15 01:05:54 -0700

    201910150105

--- src/SpiralMatrixII.java
+++ src/SpiralMatrixII.java
@@ -3,32 +3,31 @@ public class SpiralMatrixII {
 		if (n == 0) {
 			return new int[0][0];
 		}
-		int rowStart = 0;
-		int rowEnd = n - 1;
-		int colStart = 0;
-		int colEnd = n - 1;
+		int top = 0;
+		int bottom = n - 1;
+		int left = 0;
+		int right = n - 1;
 		int num = 1;
+		int dir = 0;
 		int[][] m = new int[n][n];
-		while (num <= n * n) {
-			for (int i = colStart; i <= colEnd; i++, num++) {
-				m[rowStart][i] = num;
+		while (top <= bottom && left <= right) {
+			if (dir == 0) {
+				for (int i = left; i <= right; i++) m[top][i] = num++;
+				top++;
+				dir = 1;
+			} else if (dir == 1) {
+				for (int i = top; i <= bottom; i++) m[i][right] = num++;
+				right--;
+				dir = 2;
+			} else if (dir == 2) {
+				for (int i = right; i >= left; i--) m[bottom][i] = num++;
+				bottom--;
+				dir = 3;
+			} else {
+				for (int i = bottom; i >= top; i--) m[i][left] = num++;
+				left++;
+				dir = 0;
 			}
-			rowStart++;
-
-			for (int i = rowStart; i <= rowEnd; i++, num++) {
-				m[i][colEnd] = num;
-			}
-			colEnd--;
-
-			for (int i = colEnd; i >= colStart; i--, num++) {
-				m[rowEnd][i] = num;
-			}
-			rowEnd--;
-
-			for (int i = rowEnd; i >= rowStart; i--, num++) {
-				m[i][colStart] = num;
-			}
-			colStart++;
 		}
 		return m;
 	}
