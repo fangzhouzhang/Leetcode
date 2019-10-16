e712615ee885665eec83fa16855a7ef2a35b7aa3
fangzhouzhang <zhangark93@gmail.com>
2019-10-16 10:11:54 -0700

    201910161011

--- src/RemoveInvalidParentheses.java
+++ src/RemoveInvalidParentheses.java
@@ -4,71 +4,95 @@ import java.util.List;
 import java.util.Set;
 
 public class RemoveInvalidParentheses {
+	private Set<String> set = new HashSet<>();
 	public List<String> removeInvalidParentheses(String s) {
 		List<String> res = new ArrayList<>();
-		int[] arr = new int[3];
-		getUnbalancedNumber(s, arr);
-		// dfs(arr[1], arr[2], s, 0, res);
-		// System.out.println("l " + arr[1] + " r " + arr[2]);
-		dfs1(arr[1], arr[2], s, 0, res, new StringBuilder(), 0, new HashSet<String>());
+		if (s.length() == 0) {
+			res.add("");
+			return res;
+		}
+		int[] arr = getUnpaired(s);
+		dfs(s, new StringBuilder(), 0, res, arr[1], arr[2]);
 		return res;
 	}
-	private void getUnbalancedNumber(String s, int[] arr) {
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if (ch == '(') {
+
+	private int[] getUnpaired(String s) {
+		int[] arr = new int[3];
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
 				arr[0]++;
-			} else if (ch ==')' && arr[0] > 0) {
+			} else if (c == ')' && arr[0] > 0) {
 				arr[0]--;
-			} else if (ch ==')' && arr[0] == 0) {
+			} else if (c == ')' && arr[0] == 0) {
 				arr[2]++;
 			}
 		}
 		arr[1] = arr[0];
+		return arr;
 	}
-	/**
 
-	 (()))
-	 (()))   (( ))
-	 **/
-	private void dfs1(int l, int r, String s, int idx, List<String> res, StringBuilder sb, int delta, Set<String> set) {
-		if (idx == s.length() && l == 0 && r == 0 && delta == 0) {
-
-			String ans = new String(sb);
-			if (set.add(ans)) {
-				res.add(ans);
-			}
-
+	private void dfs(String s, StringBuilder sb, int idx, List<String> res, int l, int r) {
+		if (l == 0 && r == 0 && isValid(sb) && idx == s.length()) {
+			set.add(new String(sb));
+			res.add(new String(sb));
 			return;
 		}
-		if (idx >= s.length() || l < 0 || r < 0 || delta < 0) {
+
+		if (idx == s.length()) return;
+		int prev = sb.length();
+		char c = s.charAt(idx);
+		if (Character.isLetter(c)) {
+			sb.append(c);
+			dfs(s, sb, idx + 1, res, l, r);
+			sb.setLength(prev);
 			return;
 		}
-		char ch = s.charAt(idx);
-		int prevLen = sb.length();
-		if (ch == '(') {
-			//keep
-			sb.append('(');
-			dfs1(l, r, s, idx + 1, res, sb, delta + 1, set);
-			sb.setLength(prevLen);
 
-			//delete
-			// int nextIdx = idx + 1;
-			dfs1(l - 1, r, s, idx + 1, res, sb, delta, set);
-		} else if (ch == ')') {
-			//keep
-			sb.append(')');
-			dfs1(l, r, s, idx + 1, res, sb, delta - 1, set);
-			sb.setLength(prevLen);
+		if (c == '(' && l > 0) {
+			//add '('
+			sb.append(c);
+			dfs(s, sb, idx + 1, res, l, r);
+			sb.setLength(prev);
 
-			//delete
-			// int nextIdx = idx + 1;
-			dfs1(l, r - 1, s, idx + 1, res, sb, delta, set);
-		} else {
-			//keep
-			sb.append(ch);
-			dfs1(l, r, s, idx + 1, res, sb, delta, set);
-			sb.setLength(prevLen);
+			//do not add '('
+			dfs(s, sb, idx + 1, res, l - 1, r);
+			return;
+		} else if (c == '(' && l == 0) {
+			//add '('
+			sb.append(c);
+			dfs(s, sb, idx + 1, res, l, r);
+			sb.setLength(prev);
 		}
+
+		if (c == ')' && r > 0) {
+			//add ')'
+			sb.append(c);
+			dfs(s, sb, idx + 1, res, l, r);
+			sb.setLength(prev);
+
+			//do not add ')'
+			dfs(s, sb, idx + 1, res, l, r - 1);
+			return;
+		} else if (c == ')' && r == 0) {
+			//add '('
+			sb.append(c);
+			dfs(s, sb, idx + 1, res, l, r);
+			sb.setLength(prev);
+		}
+
+
 	}
+
+	private boolean isValid(StringBuilder sb) {
+		if (set.contains(new String(sb))) return false;
+		int l = 0;
+		for (int i = 0; i < sb.length(); i++) {
+			char c = sb.charAt(i);
+			if (c == '(') l++;
+			else if (c == ')' && l > 0) l--;
+			else if (c == ')' && l == 0) return false;
+		}
+		return l == 0;
+	}
+
 }
