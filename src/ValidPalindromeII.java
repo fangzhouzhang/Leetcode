bdc945a229b3b6e2267c80e36cfc080ac709ac39
fangzhouzhang <zhangark93@gmail.com>
2019-10-17 08:48:14 -0700

    201910170848

--- src/ValidPalindromeII.java
+++ src/ValidPalindromeII.java
@@ -1,51 +1,24 @@
 public class ValidPalindromeII {
 	public boolean validPalindrome(String s) {
-		if (s == null || s.length() == 0) {
-			return true;
-		}
-		boolean isPalin = true;
-		int i = 0;
-		int j = s.length() - 1;
-		while (i <= j) {
-			if (s.charAt(i) == s.charAt(j)) {
-				i++;
-				j--;
+		if (s == null || s.length() == 0) return true;
+		char[] arr = s.toCharArray();
+		int start = 0, end = arr.length - 1;
+		while (start <= end) {
+			if (arr[start] != arr[end]) {
+				return isPalindrome(arr, start + 1, end) || isPalindrome(arr, start, end - 1);
 			} else {
-				isPalin = false;
-				break;
+				start++; end--;
 			}
 		}
+		return true;
+	}
 
-		if (isPalin == true) {
-			return true;
+	private boolean isPalindrome(char[] arr, int i, int j) {
+		while (i <= j) {
+			if (arr[i] != arr[j]) return false;
+			i++;
+			j--;
 		}
-		boolean falseBreak = false;
-		for (int skipPoint = 0; skipPoint < s.length(); skipPoint++) {
-			falseBreak = false;
-			int start = 0;
-			int end = s.length() - 1;
-			while (start <= end) {
-				if (start == skipPoint) {
-					start++;
-				} else if (end == skipPoint) {
-					end--;
-				} else if (s.charAt(start) == s.charAt(end)) {
-					start++;
-					end--;
-				} else {
-					isPalin = false;
-					falseBreak = true;
-					break;
-				}
-			}
-			if (falseBreak == false) {
-				isPalin = true;
-
-			}
-			if (isPalin == true) {
-				return true;
-			}
-		}
-		return false;
+		return true;
 	}
 }
