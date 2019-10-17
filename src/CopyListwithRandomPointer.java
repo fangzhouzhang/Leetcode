11f563bdd52330f5dca4e48764d865aa93773305
fangzhouzhang <zhangark93@gmail.com>
2019-10-16 19:37:14 -0700

    201910161937

--- src/CopyListwithRandomPointer.java
+++ src/CopyListwithRandomPointer.java
@@ -6,39 +6,39 @@ public class CopyListwithRandomPointer {
 	 * @param head: The head of linked list with a random pointer.
 	 * @return: A new head of a deep copy of the list.
 	 */
-	public RandomListNode copyRandomList(RandomListNode head) {
-		// write your code here
-		if (head == null) {
-			return null;
+	public Node copyRandomList(Node head) {
+		if (head == null) return null;
+		Node cur = head;
+		Map<Node, Node> map = new HashMap<>();
+		while (cur != null) {
+			Node tmp = new Node(cur.val);
+			map.put(cur, tmp);
+			cur = cur.next;
 		}
-		Map<RandomListNode, RandomListNode> map = new HashMap<>();
-		//map.put(head, new RandomListNode(head.label));
-		RandomListNode cur = head;
-		while (cur != null ) {
-			if (!map.containsKey(cur)) {
-				map.put(cur, new RandomListNode(cur.label));
-			}
-			if (cur.next != null) {
-				if (!map.containsKey(cur.next)) {
-					map.put(cur.next, new RandomListNode(cur.next.label));
-				}
-				map.get(cur).next = map.get(cur.next);
-			}
-			if (cur.random != null) {
-				if (!map.containsKey(cur.random)) {
-					map.put(cur.random, new RandomListNode(cur.random.label));
-				}
-				map.get(cur).random = map.get(cur.random);
-			}
 
+		cur = head;
+		while (cur != null) {
+			map.get(cur).next = map.get(cur.next);
+			map.get(cur).random = map.get(cur.random);
 			cur = cur.next;
 		}
 		return map.get(head);
 	}
 
-	private class RandomListNode {
-		int label;
- 	    RandomListNode next, random;
- 	    RandomListNode(int x) { this.label = x; }
-	}
+	private class Node {
+		public int val;
+		public Node next;
+		public Node random;
+
+		public Node() {}
+
+		public Node(int _val,Node _next,Node _random) {
+			val = _val;
+			next = _next;
+			random = _random;
+		}
+		public Node(int _val) {
+			val = _val;
+		}
+	};
 }
