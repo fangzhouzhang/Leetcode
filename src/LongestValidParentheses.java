095b0ddf43643643378140c02d8faf0469a5465c
fangzhouzhang <zhangark93@gmail.com>
2019-10-17 07:20:49 -0700

    201910170720

--- src/LongestValidParentheses.java
+++ src/LongestValidParentheses.java
@@ -7,28 +7,18 @@ public class LongestValidParentheses {
 	 * @return
 	 */
 	public int longestValidParentheses(String s) {
-		if (s.length() == 0) {
-			return 0;
-		}
+		if (s == null || s.length() == 0) return 0;
 		Stack<Integer> stack = new Stack<>();
 		stack.push(-1);
 		int max = 0;
 		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if (ch == '(') {
+			if (s.charAt(i) == '(') {
 				stack.push(i);
-			}
-			if (ch == ')') {
-				if (stack.isEmpty()) {
-					stack.push(i);
-				} else {
-					stack.pop();
-					if (stack.isEmpty()) {
-						stack.push(i);
-					} else {
-						max = Math.max(max, i - stack.peek());
-					}
-				}
+			} else {
+				if (!stack.isEmpty()) stack.pop();
+				if (stack.isEmpty()) stack.push(i);
+					//stack.peek() means the last index which is not part of current parenthesis
+				else max = Math.max(max, i - stack.peek());
 			}
 		}
 		return max;
