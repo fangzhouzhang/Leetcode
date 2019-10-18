668feb046bf82af171beb11b9e3d2df02cd98264
fangzhouzhang <zhangark93@gmail.com>
2019-10-17 22:51:13 -0700

    201910172251

--- src/ContinuousSubarraySum.java
+++ src/ContinuousSubarraySum.java
@@ -1,26 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContinuousSubarraySum {
 	public boolean checkSubarraySum(int[] nums, int k) {
-		if (nums == null || nums.length < 2) {
-			return false;
-		}
-		int[] prefix = new int[nums.length + 1];
-		getPrefix(prefix, nums);
-		for (int offset = 2; offset < prefix.length; offset++) {
-			for (int i = 0; i + offset < prefix.length; i++) {
-				if (k == 0 && (prefix[i + offset] - prefix[i] == 0)) {
-					return true;
-				} else if (k != 0 && (prefix[i + offset] - prefix[i]) % k == 0) {
-					return true;
-				}
-			}
+		if (nums == null || nums.length < 2) return false;
+		Map<Integer, Integer> map = new HashMap<>();
+		/**
+		 sum % k = rem
+		 since (a + b) % k = a % k + b % k
+		 (sum + n * k) % k = sum % k + (n * k) % k  = sum % k
+		 **/
+		map.put(0, -1);//[1, 1] k = 2
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (k != 0) sum = sum % k;
+			if (map.containsKey(sum)) {
+				if (i - map.get(sum) >= 2) return true;
+			} else map.put(sum, i);
 		}
 		return false;
-	}
-	private void getPrefix(int[] prefix, int[] nums) {
-		int temp = 0;
-		for (int i = 1; i < prefix.length; i++) {
-			temp += nums[i - 1];
-			prefix[i] = temp;
-		}
 	}
 }
