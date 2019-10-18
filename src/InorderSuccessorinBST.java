68241e0e846799b29dc0129b803fb38fb3fd7104
fangzhouzhang <zhangark93@gmail.com>
2019-10-17 17:50:24 -0700

    201910171750

--- src/InorderSuccessorinBST.java
+++ src/InorderSuccessorinBST.java
@@ -1,30 +1,14 @@
 public class InorderSuccessorinBST {
 	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-		if (root == null) {
-			return null;
+		TreeNode res = null;
+		while (root != null) {
+			if (root.val > p.val) {
+				res = root;
+				root = root.left;
+			} else {
+				root = root.right;
+			}
 		}
-		boolean[] isFind = new boolean[1];
-		boolean[] isDone = new boolean[1];
-		TreeNode[] res = new TreeNode[1];
-		dfs(root, p, isFind, res, isDone);
-		return res[0];
-	}
-	private void dfs(TreeNode root, TreeNode p, boolean[] isFind, TreeNode[] res, boolean[] isDone) {
-		if (root == null) {
-			return;
-		}
-		dfs(root.left, p, isFind, res, isDone);
-		if (isDone[0] == true) {
-			return;
-		}
-		if (isFind[0] == true) {
-			res[0] = root;
-			isDone[0] = true;
-			return;
-		}
-		if (root.val == p.val) {
-			isFind[0] = true;
-		}
-		dfs(root.right, p, isFind, res, isDone);
+		return res;
 	}
 }
