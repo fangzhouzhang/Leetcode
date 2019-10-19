9920272ce096a4d6d80c5abf2ee4991ea05adf4b
fangzhouzhang <zhangark93@gmail.com>
2019-10-19 00:39:30 -0700

    201910190039

--- src/SubarraySumEqualsK.java
+++ src/SubarraySumEqualsK.java
@@ -1,28 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumEqualsK {
 	public int subarraySum(int[] nums, int k) {
-		if (nums == null || nums.length == 0) {
-			return 0;
-		}
-		if (nums.length == 1 && nums[0] == k) {
-			return 1;
-		} else if (nums.length == 1 && nums[0] != k) {
-			return 0;
-		}
+		if (nums == null) return -1;
+		if (nums.length == 0) return 0;
 		int[] prefix = new int[nums.length + 1];
-		int temp = 0;
+		int sum = 0;
 		for (int i = 1; i < prefix.length; i++) {
-			temp += nums[i - 1];
-			prefix[i] = temp;
+			sum += nums[i - 1];
+			prefix[i] = sum;
 		}
+		Map<Integer, Integer> map = new HashMap<>();
 		int res = 0;
-		for (int i = 0; i < prefix.length - 1; i++) {
-			for (int j = i + 1; j < prefix.length; j++) {
-				if (prefix[j] - prefix[i] == k) {
-					res++;
-				}
-			}
+		for (int n : prefix) {
+			if (map.containsKey(n - k)) res += map.get(n - k);
+			map.put(n, map.getOrDefault(n, 0) + 1);
 		}
 		return res;
 	}
-
 }
