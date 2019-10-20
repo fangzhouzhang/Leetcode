9c44dc13a992149a05bb39bcff1df680892fa181
fangzhouzhang <zhangark93@gmail.com>
2019-10-20 15:18:58 -0700

    201910201518

--- src/MaximumSizeSubarraySumEqualsk.java
+++ src/MaximumSizeSubarraySumEqualsk.java
@@ -21,4 +21,25 @@ public class MaximumSizeSubarraySumEqualsk {
 		}
 		return max;
 	}
+
+	public int maxSubArrayLenI(int[] nums, int k) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		int max = 0;
+		int sum = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		int[] prefix = new int[nums.length + 1];
+		for (int i = 1; i < prefix.length; i++) {
+			sum += nums[i - 1];
+			prefix[i] = sum;
+		}
+		for (int i = 0; i < prefix.length; i++) {
+			if (map.containsKey(prefix[i] - k)) {
+				max = Math.max(max, i - map.get(prefix[i] - k));
+			}
+			if (!map.containsKey(prefix[i])) map.put(prefix[i], i);
+		}
+		return max;
+	}
 }
