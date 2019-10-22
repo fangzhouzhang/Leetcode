e5fcc00e2131935a68e6d389bbe4edd953fa929a
fangzhouzhang <zhangark93@gmail.com>
2019-10-22 16:01:40 -0700

    201910221601

--- src/ConvertSortedListtoBinarySearchTree.java
+++ src/ConvertSortedListtoBinarySearchTree.java
@@ -12,26 +12,17 @@ public class ConvertSortedListtoBinarySearchTree {
 			list.add(cur.val);
 			cur = cur.next;
 		}
-		return constructTree(list);
+		return constructTree(list, 0 , list.size() - 1);
 	}
-	private TreeNode constructTree(List<Integer> list) {
-		if (list.size() == 0) {
+
+	private TreeNode constructTree(List<Integer> list, int l, int r) {
+		if (l > r) {
 			return null;
 		}
-		int size = list.size();
-		int rootIdx = (0 + size) / 2;
+		int rootIdx = (l + r) / 2;
 		TreeNode root = new TreeNode(list.get(rootIdx));
-		List<Integer> left = new ArrayList<>();
-		List<Integer> right = new ArrayList<>();
-		for (int i = 0; i < size; i++) {
-			if (i < rootIdx) {
-				left.add(list.get(i));
-			} else if (i > rootIdx) {
-				right.add(list.get(i));
-			}
-		}
-		root.left = constructTree(left);
-		root.right = constructTree(right);
+		root.left = constructTree(list, l, rootIdx - 1);
+		root.right = constructTree(list, rootIdx + 1, r);
 		return root;
 	}
 }
