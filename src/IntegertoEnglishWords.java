407a709567c718fb91039b71d3ee260fd891e565
fangzhouzhang <zhangark93@gmail.com>
2019-10-23 16:42:29 -0700

    201910231642

--- src/IntegertoEnglishWords.java
+++ src/IntegertoEnglishWords.java
@@ -1,34 +1,31 @@
 public class IntegertoEnglishWords {
+	//http://rainykat.blogspot.com/2017/01/leetcodef-273-integer-to-english-words.html
 	private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 	private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-	private final String[] THOUSANDS = {"Billion", "Million", "Thousand", ""};
-	private final int[] radix = {1000000000, 1000000, 1000, 1};
+	private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 	public String numberToWords(int num) {
 		if (num == 0) return "Zero";
-		int i = 0;
 		String res = "";
-		for (i = 0; i < radix.length; i++) {
-			if (num / radix[i] == 0) {
-				continue;
+		int i = 0;
+		while (num > 0) {
+			if (num % 1000 != 0) {
+				res = helper(num % 1000) + THOUSANDS[i] + " " +  res;
 			}
-			res = res + " " + trans(num / radix[i]) + THOUSANDS[i];
-			num %= radix[i];
+			num /= 1000;
+			i++;
 		}
 		return res.trim();
 	}
 
-	private String trans(int num) {
+	private String helper(int num) {
 		if (num == 0) {
 			return "";
-		}
-		else if (num < 20) {
+		} else if (num < 20) {
 			return LESS_THAN_20[num % 20] + " ";
-		}
-		else if (num < 100) {
-			return TENS[num / 10] + " " + trans(num % 10);
-		}
-		else {
-			return LESS_THAN_20[num / 100] + " Hundred " + trans(num % 100);
+		} else if (num < 100) {
+			return TENS[num / 10] + " " + helper(num % 10);
+		} else {
+			return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
 		}
 	}
 }
