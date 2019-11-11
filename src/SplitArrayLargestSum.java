1e22b39639fce495d2e78daf90e68560e07b983c
fangzhouzhang <zhangark93@gmail.com>
2019-11-11 10:56:34 -0800

    201911111056

--- src/SplitArrayLargestSum.java
+++ src/SplitArrayLargestSum.java
@@ -1,31 +1,27 @@
 public class SplitArrayLargestSum {
 	public int splitArray(int[] nums, int m) {
-		long left = Integer.MIN_VALUE;
-		long right = 0;
-		for (int num : nums) {
-			left = Math.max(left, num);
-			right += num;
+		int[] prefix = new int[nums.length + 1];
+		int sum = 0;
+		for (int i = 1; i < prefix.length; i++) {
+			sum += nums[i - 1];
+			prefix[i] = sum;
 		}
-		long res = right;
-		while (left <= right) {
-			long mid = left + (right - left) / 2;
-			long sum = 0;
-			long cnt = 1;
-			for (int n : nums) {
-				if (sum + n > mid) {
-					cnt++;
-					sum = n;
-				} else {
-					sum += n;
+
+		int[][] dp = new int[m + 1][nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			dp[1][i] = prefix[i + 1];
+		}
+
+		for (int i = 2; i <= m; i++) {
+			for (int j = i - 1; j < nums.length; j++) {
+				int min = Integer.MAX_VALUE;
+				for (int k = 0; k < j; k++) {
+					int max = Math.max(dp[i - 1][k], prefix[j + 1] - prefix[k + 1]);
+					min = Math.min(min, max);
 				}
-			}
-			if (cnt <= m) {
-				res = Math.min(res, mid);
-				right = mid - 1;
-			} else {
-				left = mid + 1;
+				dp[i][j] = min;
 			}
 		}
-		return (int)res;
+		return dp[m][nums.length - 1];
 	}
 }
