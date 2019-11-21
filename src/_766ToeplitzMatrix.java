5a712f756524af2d108824f6f49b2a38864ef7cb
fangzhouzhang <zhangark93@gmail.com>
2019-11-21 11:39:57 -0800

    201911211139

--- src/_766ToeplitzMatrix.java
+++ src/_766ToeplitzMatrix.java
@@ -1,8 +1,8 @@
-public class ToeplitzMatrix {
+public class _766ToeplitzMatrix {
 	public boolean isToeplitzMatrix(int[][] matrix) {
 		for (int i = 1; i < matrix.length; i++) {
 			for (int j = 1; j < matrix[i].length; j++) {
-				if (i > 0 && j > 0 && matrix[i - 1][j - 1] != matrix[i][j]) return false;
+				if (matrix[i - 1][j - 1] != matrix[i][j]) return false;
 			}
 		}
 		return true;
