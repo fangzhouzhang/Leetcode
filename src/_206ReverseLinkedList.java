6c70c2e8fee9a6a1b299e54110a3a5b36dce463c
fangzhouzhang <zhangark93@gmail.com>
2019-11-25 22:24:33 -0800

    201911252224

---
+++ src/_206ReverseLinkedList.java
@@ -0,0 +1,32 @@
+public class _206ReverseLinkedList {
+    /**
+     * @param head: n
+     * @return: The new head of reversed linked list.
+     */
+	public ListNode reverseList(ListNode head) {
+		if (head == null || head.next == null) return head;
+		ListNode tmp = head.next;
+		ListNode newHead = reverseList(head.next);
+		tmp.next = head;
+		head.next = null;
+		return newHead;
+	}
+
+	/**
+	 * non-recursive
+	 * time: o(n) space: o(1)
+	 * @param head
+	 * @return
+	 */
+	public ListNode reverseList1(ListNode head) {
+		if (head == null || head.next == null) return head;
+		ListNode pre = null, cur = head;
+		while (cur != null) {
+			ListNode tmp = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = tmp;
+		}
+		return pre;
+	}
+}
