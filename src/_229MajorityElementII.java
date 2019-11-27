90353af9a9e182475b7c004a4823bf025736d074
fangzhouzhang <zhangark93@gmail.com>
2019-11-27 14:15:38 -0800

    201911271415

---
+++ src/_229MajorityElementII.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _229MajorityElementII {
+    /**
+     * @param nums: a list of integers
+     * @return: The majority number that occurs more than 1/3
+     * time: o(n) space: o(1)
+     */
+	public List<Integer> majorityElement(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		if (nums == null || nums.length == 0) return res;
+		//c stands for candidata, f stands for frequency
+		Integer c1 = null, c2 = null;
+		int f1 = 0, f2 = 0;
+		for (int n : nums) {
+			if (c1 != null && c1 == n) f1++;
+			else if (c2 != null && c2 == n) f2++;
+			else if (c1 == null || f1 == 0) {//[2,2]
+				c1 = n;
+				f1 = 1;
+			} else if (c2 == null || f2 == 0) {
+				c2 = n;
+				f2 = 1;
+			} else if (c1 != null && c1 != n && c2 != null && c2 != n) {// [4,2,3,1]
+				f1--;
+				f2--;
+			}
+		}
+
+		f1 = 0;
+		f2 = 0;
+		for (int n : nums) {
+			if (c1 != null && c1 == n) f1++;
+			if (c2 != null && c2 == n) f2++;
+		}
+		if (f1 > nums.length / 3) res.add(c1);
+		if (f2 > nums.length / 3) res.add(c2);
+		return res;
+	}
+
+}
+
