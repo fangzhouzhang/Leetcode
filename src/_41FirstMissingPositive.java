81dfaf9c07b8326b5d6fd6c2e2d838b8855da878
fangzhouzhang <zhangark93@gmail.com>
2019-11-30 14:22:24 -0800

    201911301422

---
+++ src/_41FirstMissingPositive.java
@@ -0,0 +1,15 @@
+public class _41FirstMissingPositive {
+	public int firstMissingPositive(int[] nums) {
+		if (nums == null || nums.length == 0) return 1;
+		for (int i = 0; i < nums.length; i++) {
+			while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1]!= nums[i]) {
+				//follow up: can we exchange sequence here?
+				int tmp = nums[nums[i] - 1];
+				nums[nums[i] - 1] = nums[i];
+				nums[i] = tmp;
+			}
+		}
+		for (int i = 0; i < nums.length; i++) if (nums[i] != i + 1) return i + 1;
+		return nums.length + 1;
+	}
+}
