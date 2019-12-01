beb32cf379ce55611c1dff1ce3725325030b7404
fangzhouzhang <zhangark93@gmail.com>
2019-12-01 13:51:00 -0800

    201912011350

--- src/_285InorderSuccessorinBST.java
+++ src/_285InorderSuccessorinBST.java
@@ -1,13 +1,12 @@
-public class InorderSuccessorinBST {
+public class _285InorderSuccessorinBST {
 	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+		if (root == null || p == null) return null;
 		TreeNode res = null;
 		while (root != null) {
 			if (root.val > p.val) {
 				res = root;
 				root = root.left;
-			} else {
-				root = root.right;
-			}
+			} else root = root.right;
 		}
 		return res;
 	}
