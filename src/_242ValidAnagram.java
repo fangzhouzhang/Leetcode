26e1c7f9e8e640d492eb3ca705f0f81776e65e58
fangzhouzhang <zhangark93@gmail.com>
2019-12-10 08:34:40 -0800

    201912100834

---
+++ src/_242ValidAnagram.java
@@ -0,0 +1,13 @@
+public class _242ValidAnagram {
+	public boolean isAnagram(String s, String t) {
+		if (s == null || t == null) return false;
+		if (s.length() != t.length()) return false;
+		int[] map = new int[256];
+		for (char c : s.toCharArray()) map[c]++;
+		for (char c : t.toCharArray()) {
+			map[c]--;
+			if (map[c] < 0) return false;
+		}
+		return true;
+	}
+}
