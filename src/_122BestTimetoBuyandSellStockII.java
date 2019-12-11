0daa3c3668a81769aeeffdac0fe4bd38f973ee41
fangzhouzhang <zhangark93@gmail.com>
2019-12-10 20:18:23 -0800

    201912102018

---
+++ src/_122BestTimetoBuyandSellStockII.java
@@ -0,0 +1,11 @@
+public class _122BestTimetoBuyandSellStockII {
+	public int maxProfit(int[] prices) {
+		if (prices == null || prices.length == 0) return 0;
+		int n = prices.length;
+		int res = 0;
+		for (int i = 1; i < n; i++) {
+			if (prices[i] > prices[i - 1]) res += prices[i] - prices[i - 1];
+		}
+		return res;
+	}
+}
