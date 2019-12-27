d73b2e41da26df0a2187ce0a76ec74806e2380bc
fangzhouzhang <zhangark93@gmail.com>
2019-12-27 14:34:16 -0800

    201912271434

---
+++ src/_209MinimumSizeSubarraySum.java
@@ -0,0 +1,16 @@
+public class _209MinimumSizeSubarraySum {
+	public int minSubArrayLen(int s, int[] nums) {
+		if (nums == null || nums.length == 0) return 0;
+		int sum = 0, start = 0, end = 0, min = Integer.MAX_VALUE;
+		while (end < nums.length) {
+			sum += nums[end];
+			while (sum >= s) {
+				if (min > end - start) min = end - start + 1;
+				sum -= nums[start];
+				start++;
+			}
+			end++;
+		}
+		return min == Integer.MAX_VALUE ? 0 : min;
+	}
+}
