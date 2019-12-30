984133f1f96f7fc6ec3359cf7be8b9604b3e7efb
fangzhouzhang <zhangark93@gmail.com>
2019-12-29 23:29:28 -0800

    201912292329

--- src/_410SplitArrayLargestSum.java
+++ src/_410SplitArrayLargestSum.java
@@ -1,28 +1,28 @@
 public class _410SplitArrayLargestSum {
 	public int splitArray(int[] nums, int m) {
-		int[] prefix = new int[nums.length + 1];
+		if (nums == null || nums.length == 0) return 0;
+		int n = nums.length;
+		int[] prefix = new int[n + 1];
 		int sum = 0;
-		for (int i = 1; i < prefix.length; i++) {
-			sum += nums[i - 1];
-			prefix[i] = sum;
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			prefix[i + 1] = sum;
 		}
 
-		int[][] dp = new int[m + 1][nums.length];
-		for (int i = 0; i < nums.length; i++) {
-			dp[1][i] = prefix[i + 1];
-		}
+		int[][] dp = new int[m + 1][n];
+		for (int i = 0; i < n; i++) dp[1][i] = prefix[i + 1];
 
-		for (int i = 2; i <= m; i++) {
-			for (int j = i - 1; j < nums.length; j++) {
+		for (int i = 2; i < dp.length; i++) {
+			for (int j = i - 1; j < n; j++) {
 				int min = Integer.MAX_VALUE;
-				for (int k = 0; k < j; k++) {
-					int max = Math.max(dp[i - 1][k], prefix[j + 1] - prefix[k + 1]);
-					min = Math.min(min, max);
+				for (int k = i - 1; k <= j; k++) {
+					int curMax = Math.max(dp[i - 1][k - 1], prefix[j + 1] - prefix[k]);
+					min = Math.min(min, curMax);
 				}
 				dp[i][j] = min;
 			}
 		}
-		return dp[m][nums.length - 1];
+		return dp[m][n - 1];
 	}
 
 	public int splitArray1(int[] nums, int m) {
