2923bf9c75dc98853e64bbdbdab16dd9780049bb
fangzhouzhang <zhangark93@gmail.com>
2019-12-29 23:59:14 -0800

    201912292359

--- src/_523ContinuousSubarraySum.java
+++ src/_523ContinuousSubarraySum.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class ContinuousSubarraySum {
+public class _523ContinuousSubarraySum {
 	public boolean checkSubarraySum(int[] nums, int k) {
 		if (nums == null || nums.length < 2) return false;
 		Map<Integer, Integer> map = new HashMap<>();
@@ -10,14 +10,13 @@ public class ContinuousSubarraySum {
 		 since (a + b) % k = a % k + b % k
 		 (sum + n * k) % k = sum % k + (n * k) % k  = sum % k
 		 **/
-		map.put(0, -1);//[1, 1] k = 2
+		map.put(0, 0);
 		int sum = 0;
 		for (int i = 0; i < nums.length; i++) {
 			sum += nums[i];
-			if (k != 0) sum = sum % k;
-			if (map.containsKey(sum)) {
-				if (i - map.get(sum) >= 2) return true;
-			} else map.put(sum, i);
+			if (k != 0) sum %= k;
+			if (map.containsKey(sum) && i + 1 - map.get(sum) >= 2) return true;
+			if (!map.containsKey(sum)) map.put(sum, i + 1);
 		}
 		return false;
 	}
