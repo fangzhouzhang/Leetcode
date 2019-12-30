e731f91dbe0718bff9a4861b60e170e6ec88cd84
fangzhouzhang <zhangark93@gmail.com>
2019-12-30 15:58:58 -0800

    201912301558

---
+++ src/_344ReverseString.java
@@ -0,0 +1,13 @@
+public class _344ReverseString {
+	public void reverseString(char[] s) {
+		if (s == null || s.length == 0) return;
+		int i = 0, j = s.length - 1;
+		while (i < j) {
+			char tmp = s[i];
+			s[i] = s[j];
+			s[j] = tmp;
+			i++;
+			j--;
+		}
+	}
+}
