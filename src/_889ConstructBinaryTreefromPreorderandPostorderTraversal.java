e576fcea8f668bdf7d454d4bbaea47cc700f6f4d
fangzhouzhang <zhangark93@gmail.com>
2019-12-31 15:01:55 -0800

    201912311501

--- src/_889ConstructBinaryTreefromPreorderandPostorderTraversal.java
+++ src/_889ConstructBinaryTreefromPreorderandPostorderTraversal.java
@@ -1,22 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class _889ConstructBinaryTreefromPreorderandPostorderTraversal {
 	public TreeNode constructFromPrePost(int[] pre, int[] post) {
-		return dfs(pre, 0, post, 0, pre.length);
+		if (pre == null || pre.length == 0 || post == null || post.length == 0) return null;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < post.length; i++) map.put(post[i], i);
+		return dfs(pre, 0, post, 0, pre.length, map);
 	}
 
-	private TreeNode dfs(int[] pre, int i, int[] post, int j, int n) {
-		if (n <= 0) return null;
-		if (n == 1) return new TreeNode(pre[i]);
-
-		int target = pre[i + 1];
-		int startIdx = j;
-		int endIdx = j;
-		while (endIdx < post.length && post[endIdx] != target) {
-			endIdx++;
-		}
-		int leftLen = endIdx - startIdx + 1;
-		TreeNode root = new TreeNode(pre[i]);
-		root.left = dfs(pre, i + 1, post, j, leftLen);
-		root.right = dfs(pre, i + 1 + leftLen, post, endIdx + 1, n - 1 - leftLen);
+	private TreeNode dfs(int[] pre, int preIdx, int[] post, int postIdx, int toBeProcess, Map<Integer, Integer> map) {
+		if (toBeProcess == 0) return null;
+		TreeNode root = new TreeNode(pre[preIdx]);
+		if (toBeProcess == 1) return root;
+		int target = pre[preIdx + 1];
+		int leftSize = map.get(target) - postIdx + 1;
+		root.left = dfs(pre, preIdx + 1, post, postIdx, leftSize, map);
+		root.right = dfs(pre, preIdx + 1 + leftSize, post, postIdx + leftSize, toBeProcess - 1 - leftSize, map);
 		return root;
 	}
 }
