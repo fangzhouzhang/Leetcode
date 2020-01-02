c8d1552b8b1687a7aaa06cd881f722e818b5c20e
fangzhouzhang <zhangark93@gmail.com>
2020-01-02 15:32:42 -0800

    202001021532

--- src/_1057CampusBikes.java
+++ src/_1057CampusBikes.java
@@ -3,34 +3,34 @@ import java.util.List;
 
 public class _1057CampusBikes {
 	public int[] assignBikes(int[][] workers, int[][] bikes) {
-		if (workers == null || bikes == null) return null;
+		if (workers == null || workers.length == 0 || bikes == null || bikes.length == 0) return new int[0];
 		int n = workers.length, m = bikes.length;
-		int[] res = new int[n];
-		List<List<int[]>> list = new ArrayList<>(2001);
-		for (int i = 0; i < 2001; i++) list.add(new ArrayList<>());
+		List[] distances = new List[2001];
+		for (int i = 0; i < 2001; i++) distances[i] = new ArrayList<int[]>();
+		boolean[] assignWorkers = new boolean[n];
+		boolean[] assignBikes = new boolean[m];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				int dist = getDistance(workers, i, bikes, j);
-				list.get(dist).add(new int[]{i, j});
+				int dis = getDistance(workers[i], bikes[j]);
+				distances[dis].add(new int[]{i, j});
 			}
 		}
-
-		boolean[] usedWorker = new boolean[n];
-		boolean[] usedBike = new boolean[m];
-
-		for (List<int[]> l : list) {
-			for (int[] p : l) {
-				if (!usedWorker[p[0]] && !usedBike[p[1]]) {
-					res[p[0]] = p[1];
-					usedWorker[p[0]] = true;
-					usedBike[p[1]] = true;
+		int[] res = new int[n];
+		for (int i = 0; i < 2001; i++) {
+			List<int[]> list = distances[i];
+			if (list == null) continue;
+			for (int[] pair : list) {
+				if (!assignWorkers[pair[0]] && !assignBikes[pair[1]]) {
+					assignWorkers[pair[0]] = true;
+					assignBikes[pair[1]] = true;
+					res[pair[0]] = pair[1];
 				}
 			}
 		}
 		return res;
 	}
 
-	private int getDistance(int[][] workers, int i, int[][] bikes, int j) {
-		return Math.abs(workers[i][0] - bikes[j][0]) + Math.abs(workers[i][1] - bikes[j][1]);
+	private int getDistance(int[] a, int[] b) {
+		return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
 	}
 }
