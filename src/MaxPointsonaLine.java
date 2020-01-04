805710ff82268587bd1185e045eb49af60e5a554
fangzhouzhang <zhangark93@gmail.com>
2020-01-04 11:16:55 -0800

    202001041116

--- src/MaxPointsonaLine.java
+++ src/MaxPointsonaLine.java
@@ -9,93 +9,54 @@ public class MaxPointsonaLine {
 	 * @return: An integer
 	 * time: o(n^2) space: o(n)
 	 */
-	public int maxPoints(Point[] points) {
-		if (points.length <= 1) {
-			return points.length;
-		}
-
-		Map<Slope, Integer> map = new HashMap<>();
-		int[] global = new int[1];
-		for (int i = 0; i < points.length; i++) {
-			calcMax(points, i, map, global);
-
-		}
-
-		return global[0];
-	}
-	private void calcMax(Point[] points, int i, Map<Slope, Integer> map, int[] global) {
-		int local = 0;
-		int same = 0;
-		for (int j = 0; j < points.length; j++) {
-			// if (i == j) {
-			//     continue;
-			// }
-			if (points[i].x == points[j].x && points[i].y == points[j].y) {
-				same++;
-				continue;
+	private final String VERTICAL = "VERTICAL";
+	private final String HORIZONTAL = "HORIZONTAL";
+	public int maxPoints(int[][] points) {
+		if (points == null || points.length == 0) return 0;
+		Set<String> set = new HashSet<>();
+		int max = 1;
+		for (int i = 0; i < points.length && !set.contains(points[i][0] + "#" + points[i][1]); i++) {
+			int[] a = points[i];
+			int localMax = 1;
+			int same = 0;
+			Map<String, Integer> map = new HashMap<>();
+			for (int j = i + 1; j < points.length; j++) {
+				int[] b = points[j];
+				if (a[0] == b[0] && a[1] == b[1]) {
+					same++;
+					continue;
+				}
+				int y = a[1] - b[1];
+				int x = a[0] - b[0];
+				String slope = null;
+				if (y == 0) {
+					slope = VERTICAL;
+				} else if (x == 0) {
+					slope = HORIZONTAL;
+				} else {
+					int gcd = getGcd(y, x);
+					if (gcd != 0) {
+						y /= gcd;
+						x /= gcd;
+					}
+					if ((x > 0 && y > 0) || (x > 0 && y > 0)) {
+						slope = y + "#" + x;
+					} else {
+						slope = "-" + y + "#" + x;
+					}
+				}
+				map.put(slope, map.getOrDefault(slope, 1) + 1);
+				localMax = Math.max(localMax, map.get(slope));
 			}
-			int cur = 0;
-			if (points[i].x == points[j].x) {
-				cur = addMap(map, 0, points[i].x);
-			} else if (points[i].y == points[j].y) {
-				cur = addMap(map, points[i].y, 0);
-			} else {
-				cur = addMap(map, points[i].y - points[j].y, points[i].x - points[j].x);
-			}
-			// System.out.println(local + " " + cur);
-			local = Math.max(local, cur);
+			max = Math.max(max, localMax + same);
+			set.add((points[i][0] + "#" + points[i][1]));
 		}
-		map.clear();
-		global[0] = Math.max(global[0], local + same);
-
-	}
-	private int addMap(Map<Slope, Integer> map, int dy, int dx) {
-		Slope slope = new Slope(dy, dx);
-		if (map.containsKey(slope)) {
-			map.put(slope, map.get(slope) + 1);
-		} else {
-			map.put(slope, 1);
-		}
-		return map.get(slope);
-	}
-	private class Slope {
-		private int dx;
-		private int dy;
-		public Slope(int dy, int dx) {
-			if (dy == 0 || dx == 0) {
-				this.dy = dy;
-				this.dx = dx;
-			} else {
-				int num = gcd(dy, dx);
-				// System.out.println(dy + "  " + dx + "  " + num);
-				this.dy = dy / num;
-				this.dx = dx / num;
-			}
-
-		}
-		public boolean equals(Object obj) {
-			if (obj == null) {
-				return false;
-			}
-			Slope s = (Slope)obj;
-			if (this.dy != s.dy || this.dx != s.dx) {
-				return false;
-			}
-			return true;
-		}
-		public int hashCode() {
-			return 1;
-		}
+		return max;
 	}
 
-	private int gcd(int m, int n) {
-		return n == 0 ? m : gcd(n, m % n);
-	}
-	class Point {
-		int x;
-		int y;
-		Point() { x = 0; y = 0; }
-		Point(int a, int b) { x = a; y = b; }
+	private int getGcd(int y, int x) {
+		if (x == 0) return y;
+		return getGcd(x, (y % x));
 	}
 }
 
