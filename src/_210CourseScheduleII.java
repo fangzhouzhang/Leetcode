10f0ac8de54385b38d222afadc90b8a936e05643
fangzhouzhang <zhangark93@gmail.com>
2020-01-04 01:36:36 -0800

    202001040136

--- src/_210CourseScheduleII.java
+++ src/_210CourseScheduleII.java
@@ -5,49 +5,31 @@ public class _210CourseScheduleII {
 	 * time: o(v + e) e [v, v^2]
 	 * 
 	 */
-	private final int INIT = 0;
-	private final int VISITING = 1;
-	private final int VISITED = 2;
 	public int[] findOrder(int numCourses, int[][] prerequisites) {
-		if (numCourses < 0) return new int[0];
-		int[] res = new int[numCourses];
-		if (prerequisites == null || prerequisites.length == 0) {
-			for (int i = 0; i < numCourses; i++) res[i] = i;
-		}
-		Node[] nodes = new Node[numCourses];
-		for (int i = 0; i < numCourses; i++) nodes[i] = new Node(i);
+		if (numCourses == 0 || prerequisites == null) return new int[0];
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		int[] indegrees = new int[numCourses];
 		for (int[] p : prerequisites) {
-			nodes[p[1]].neighbors.add(nodes[p[0]]);
+			indegrees[p[0]]++;
+			if (!map.containsKey(p[1])) map.put(p[1], new ArrayList<Integer>());
+			map.get(p[1]).add(p[0]);
 		}
-		int[] idx = new int[1];
-		idx[0] = numCourses - 1;
-		for (Node n : nodes) {
-			if (containCycle(n, idx, res)) return new int[0];
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 0; i < numCourses; i++) {
+			if (indegrees[i] == 0) q.add(i);
 		}
-		return res;
-	}
-
-	private boolean containCycle(Node node, int[] idx, int[] res) {
-		if (node.status == VISITING) return true;
-		if (node.status == VISITED) return false;
-		node.status = VISITING;
-		for (Node n : node.neighbors) {
-			if (containCycle(n, idx, res)) return true;
+		int[] res = new int[numCourses];
+		int idx = 0;
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			res[idx++] = cur;
+			if (!map.containsKey(cur) || map.get(cur).size() == 0) continue;
+			for (int n : map.get(cur)) {
+				indegrees[n]--;
+				if (indegrees[n] == 0) q.add(n);
+			}
 		}
-		node.status = VISITED;
-		res[idx[0]] = node.val;
-		idx[0]--;
-		return false;
-	}
-
-	private class Node {
-		int val;
-		int status;
-		Set<Node> neighbors;
-		public Node(int val) {
-			this.val = val;
-			status = INIT;
-			neighbors = new HashSet<>();
-		}
+		if (idx == numCourses) return res;
+		return new int[0];
 	}
 }
