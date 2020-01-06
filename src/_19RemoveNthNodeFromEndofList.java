b44e8b37d20c745971f519e4870de88bacab19cd
fangzhouzhang <zhangark93@gmail.com>
2020-01-06 12:51:49 -0800

    202001061251

--- src/_19RemoveNthNodeFromEndofList.java
+++ src/_19RemoveNthNodeFromEndofList.java
@@ -1,30 +1,19 @@
-public class RemoveNthNodeFromEndofList {
+public class _19RemoveNthNodeFromEndofList {
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-		if (head == null) {
-			return head;
-		}
-		ListNode dummy = new ListNode(-1);
+		if (head == null) return head;
+		ListNode dummy = new ListNode(0);
 		dummy.next = head;
-
 		ListNode fast = dummy;
 		ListNode slow = dummy;
-
-		int cnt = 0;
-
-		while (fast != null && cnt < n + 1) {
-			fast = fast.next;
-			cnt++;
-		}
-
+		int k = 0;
+		while (k++ < n + 1) fast = fast.next;
 		while (fast != null) {
 			fast = fast.next;
 			slow = slow.next;
 		}
-
 		ListNode del = slow.next;
 		slow.next = del.next;
 		del.next = null;
-
 		return dummy.next;
 	}
 }
