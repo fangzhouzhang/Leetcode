285ba5fb9b7b94aa16f8384eeaf7e3d11a15a9d3
fangzhouzhang <zhangark93@gmail.com>
2020-01-25 09:05:32 -0800

    202001250905

--- src/_1109CorporateFlightBookings.java
+++ src/_1109CorporateFlightBookings.java
@@ -2,13 +2,15 @@ public class _1109CorporateFlightBookings {
 	public int[] corpFlightBookings(int[][] bookings, int n) {
 		if (bookings == null || bookings.length == 0) return new int[0];
 		int[] res = new int[n];
+		int[] diff = new int[n + 2];
 		for (int[] b : bookings) {
-			int start = b[0];
-			int end = b[1];
-			int num = b[2];
-			for (int i = start; i <= end; i++) {
-				res[i - 1] += num;
-			}
+			diff[b[0]] += b[2];
+			diff[b[1] + 1] -= b[2];
+		}
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += diff[i];
+			res[i - 1] = sum;
 		}
 		return res;
 	}
