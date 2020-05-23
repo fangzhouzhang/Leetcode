990b26234c35ee304fd7f53e597e9a90c48477dd
fangzhouzhang <zhangark93@gmail.com>
2020-05-23 10:47:34 -0700

    202005231047

--- src/_159LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ src/_159LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -1,22 +1,21 @@
 public class _159LongestSubstringwithAtMostTwoDistinctCharacters {
 	public int lengthOfLongestSubstringTwoDistinct(String s) {
-		if (s.length() == 0) return 0;
-		int[] map = new int[128];
-		int cnt = 0, max = -1;
+		if (s == null || s.length() == 0) return 0;
 		int slow = 0, fast = 0;
+		int res = 1;
+		int[] map = new int[256];
+		int unique = 0;
 		while (fast < s.length()) {
-			char c = s.charAt(fast);
-			map[c]++;
-			if (map[c] == 1) cnt++;
-			while (cnt > 2) {
-				char tmp = s.charAt(slow);
-				map[tmp]--;
-				if (map[tmp] == 0) cnt--;
+			map[s.charAt(fast)]++;
+			if (map[s.charAt(fast)] == 1) unique++;
+			while (slow < fast && unique > 2) {
+				map[s.charAt(slow)]--;
+				if (map[s.charAt(slow)] == 0) unique--;
 				slow++;
 			}
-			max = Math.max(max, fast - slow + 1);
+			res = Math.max(res, fast - slow + 1);
 			fast++;
 		}
-		return max;
+		return res;
 	}
 }
