cdb659e2c2b6624be95c4c3c4613e4148cc38bba
fangzhouzhang <zhangark93@gmail.com>
2020-05-23 19:57:13 -0700

    202005231957

--- src/_163MissingRanges.java
+++ src/_163MissingRanges.java
@@ -4,21 +4,27 @@ import java.util.List;
 public class _163MissingRanges {
 	public List<String> findMissingRanges(int[] nums, int lower, int upper) {
 		List<String> res = new ArrayList<>();
-		if (nums == null) return res;
-		if (nums.length == 0) {
-			res.add(getInterval(lower, upper));
+		if (nums == null || nums.length == 0) {
+			addInterval(lower, upper, res);
 			return res;
 		}
-		if (lower < nums[0]) res.add(getInterval(lower, nums[0] - 1));
-		int n = nums.length;
-		for (int i = 0; i < n - 1; i++) {
-			if ((long)nums[i + 1] > (long)nums[i] + 1) res.add(getInterval(nums[i] + 1, nums[i + 1] - 1));
+		long start = (long)lower, end = 0;
+		for (int n : nums) {
+			end = (long)n - 1;
+			if (start <= end) addInterval((int)start, (int)end, res);
+			start = (long)n + 1;
 		}
-		if (nums[n - 1] < upper) res.add(getInterval(nums[n - 1] + 1, upper));
+		end = (long)upper;
+		if (start <= end) addInterval((int)start, (int)end, res);
 		return res;
 	}
 
-	private String getInterval(int low, int high) {
-		return low == high ? "" + low : low + "->" + high;
+	private void addInterval(int s, int e, List<String> res) {
+		if (s == e) {
+			res.add(s + "");
+		} else {
+			String cur = s + "->" + e;
+			res.add(cur);
+		}
 	}
 }
