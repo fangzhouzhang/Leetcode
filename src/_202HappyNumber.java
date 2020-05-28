086affedda4ea11456767c3cd1b22000ac039212
fangzhouzhang <zhangark93@gmail.com>
2020-05-28 10:11:21 -0700

    202005281011

--- src/_202HappyNumber.java
+++ src/_202HappyNumber.java
@@ -5,17 +5,20 @@ public class _202HappyNumber {
 	public boolean isHappy(int n) {
 		if (n <= 0) return false;
 		Set<Integer> set = new HashSet<>();
-		while (n != 1) {
-			int sum = 0;
-			int current = n;
-			while (current != 0) {
-				sum += (current % 10) * (current % 10);
-				current /= 10;
+		while (true) {
+			int res = 0;
+			while (n != 0) {
+				int digit = n % 10;
+				res += digit * digit;
+				n /= 10;
 			}
-			if (set.contains(sum)) return false;
-			set.add(sum);
-			n = sum;
+			if (res == 1) return true;
+			if (set.contains(res)) {
+				return false;
+			} else {
+				set.add(res);
+				n = res;
+			}
 		}
-		return true;
 	}
 }
