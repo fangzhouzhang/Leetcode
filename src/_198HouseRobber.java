aef7f34829126e6efdf3dd7bf0279b3c4cdf0c51
fangzhouzhang <zhangark93@gmail.com>
2020-05-28 08:22:43 -0700

    202005280822

--- src/_198HouseRobber.java
+++ src/_198HouseRobber.java
@@ -1,15 +1,15 @@
 public class _198HouseRobber {
 	public int rob(int[] nums) {
-		if (nums == null || nums.length == 0) return 0;
+		if (null == nums || nums.length == 0) return 0;
 		Integer[] dp = new Integer[nums.length];
-		return dfs(nums, nums.length - 1, dp);
+		return dfs(dp, nums, 0);
 	}
 
-	private int dfs(int[] nums, int idx, Integer[] dp) {
-		if (idx < 0) return 0;
+	private int dfs(Integer[] dp, int[] nums, int idx) {
+		if (idx >= nums.length) return 0;
 		if (dp[idx] != null) return dp[idx];
-		int max = Math.max(dfs(nums, idx - 1, dp), dfs(nums, idx - 2, dp) + nums[idx]);
-		dp[idx] = max;
-		return max;
+		int res = Math.max(dfs(dp, nums, idx + 2) + nums[idx], dfs(dp, nums, idx + 1));
+		dp[idx] = res;
+		return res;
 	}
 }
