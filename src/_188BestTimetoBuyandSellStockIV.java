2eb55539d44e611cbba3eeadca536c88a1d06dc4
fangzhouzhang <zhangark93@gmail.com>
2020-05-27 19:34:39 -0700

    202005271934

--- src/_188BestTimetoBuyandSellStockIV.java
+++ src/_188BestTimetoBuyandSellStockIV.java
@@ -2,25 +2,31 @@ public class _188BestTimetoBuyandSellStockIV {
 	public int maxProfit(int k, int[] prices) {
 		if (prices == null || prices.length == 0) return 0;
 		if (k > prices.length / 2) {
-			int sum = 0;
+			int res = 0;
 			for (int i = 1; i < prices.length; i++) {
-				if (prices[i] > prices[i - 1]) sum += prices[i] - prices[i - 1];
+				if (prices[i] > prices[i - 1]) {
+					res += prices[i] - prices[i - 1];
+				}
 			}
-			return sum;
+			return res;
 		}
-		int[] tmp = new int[prices.length];
-		int[] pre = new int[prices.length];
-		int[] cur = new int[prices.length];
+
+		long[] b = new long[k + 1];
+		long[] s = new long[k + 1];
 		for (int i = 1; i <= k; i++) {
-			int localMax = pre[0] - prices[0];
-			for (int j = 1; j < prices.length; j++) {
-				cur[j] = Math.max(cur[j - 1], prices[j] + localMax);
-				localMax = Math.max(localMax, pre[j] - prices[j]);
-			}
-			tmp = pre;
-			pre = cur;
-			cur = tmp;
+			b[i] = Integer.MIN_VALUE;
 		}
-		return pre[prices.length - 1];
+
+		for (int p : prices) {
+			for (int i = k; i >= 1; i--) {
+				s[i] = Math.max(s[i], b[i] + p);
+				if (i == 1) b[i] = Math.max(b[i], -p);
+				else b[i] = Math.max(b[i], s[i - 1] - p);
+			}
+		}
+		long res = Integer.MIN_VALUE;
+		for (long n : s) res = Math.max(res, n);
+		for (long n : b) res = Math.max(res, n);
+		return (int)res;
 	}
 }
