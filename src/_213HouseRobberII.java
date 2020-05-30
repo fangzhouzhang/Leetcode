a72cf5d7ac3c84d28e3349090ab627e663ea8b4a
fangzhouzhang <zhangark93@gmail.com>
2020-05-30 13:19:25 -0700

    202005301319

--- src/_213HouseRobberII.java
+++ src/_213HouseRobberII.java
@@ -1,20 +1,20 @@
 public class _213HouseRobberII {
 	public int rob(int[] nums) {
 		if (nums == null || nums.length == 0) return 0;
-		if (nums.length == 1) return nums[0];
-		if (nums.length == 2) return Math.max(nums[0], nums[1]);
 		int n = nums.length;
-		int first = dfs(nums, 0, n - 2, new Integer[n]);
-		int second = dfs(nums, 1, n - 1, new Integer[n]);
-		return Math.max(first, second);
+		if (n == 1) return nums[0];
+		Integer[] dp = new Integer[n];
+		Integer[] dp1 = new Integer[n];
+		int m1 = dfs(nums, 0, n - 1, dp);
+		int m2 = dfs(nums, 1, n, dp1);
+		return Math.max(m1, m2);
 	}
 
-	private int dfs(int[] nums, int start, int end, Integer[] dp) {
-		if (start >= end + 1) return 0;
-		if (dp[start] != null) return dp[start];
-		int val1 = dfs(nums, start + 2, end, dp) + nums[start];
-		int val2 = dfs(nums, start + 1, end, dp);
-		dp[start] = Math.max(val1, val2);;
-		return dp[start];
+	private int dfs(int[] nums, int idx, int n, Integer[] dp) {
+		if (idx >= n) return 0;
+		if (dp[idx] != null) return dp[idx];
+		int res = Math.max(dfs(nums, idx + 2, n, dp) + nums[idx], dfs(nums, idx + 1, n, dp));
+		dp[idx] = res;
+		return res;
 	}
 }
