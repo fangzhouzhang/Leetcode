04594772a6db2a06f2a25909f20f6cf10ad50f9b
fangzhouzhang <zhangark93@gmail.com>
2020-05-30 13:08:31 -0700

    202005301308

--- src/_212WordSearchII.java
+++ src/_212WordSearchII.java
@@ -9,59 +9,69 @@ public class _212WordSearchII {
 	 * board is m * n, length of one word is l, size of words is k
 	 * time: o(k * m * n * 4^l) space: call stack o(l)
 	 */
+	TrieNode root = new TrieNode();
 	private int[][] dirs = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
 	public List<String> findWords(char[][] board, String[] words) {
 		List<String> res = new ArrayList<>();
-		if (board == null || board.length == 0 || board[0].length == 0 || words == null || words.length == 0) return res;
-		TrieNode root = new TrieNode();
-		for (String word : words) buildTrie(root, word);
+		if (null == board || board.length == 0 || words == null || words.length == 0) return res;
+		for (String word : words) buildTrie(word);
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				dfs(board, i, j, res, root);
+				int idx = board[i][j] - 'a';
+				if (root.children[idx] != null) {
+					dfs(root, board, i, j, new StringBuilder(), res);
+				}
 			}
 		}
 		return res;
 	}
 
-	private void dfs(char[][] board, int row, int col, List<String> res, TrieNode root) {
-		char c = board[row][col];
-		if (root.children[c - 'a'] == null) return;
-		if (root.children[c - 'a'].word != null) {
-			res.add(root.children[c - 'a'].word);
-			root.children[c - 'a'].word = null;
-		}
-		board[row][col] = '#';
-		for (int[] dir : dirs) {
-			int newRow = row + dir[0];
-			int newCol = col + dir[1];
-			if (isValid(board, newRow, newCol)) dfs(board, newRow, newCol, res, root.children[c - 'a']);
-		}
-		board[row][col] = c;
-	}
-
 	private boolean isValid(char[][] board, int i, int j) {
-		if (i < 0 || i >= board.length) return false;
-		if (j < 0 || j >= board[i].length) return false;
-		if (board[i][j] == '#') return false;
+		if (i < 0 || i >= board.length) {
+			return false;
+		}
+		if (j < 0 || j >= board[i].length) {
+			return false;
+		}
+		if (board[i][j] == '#') {
+			return false;
+		}
 		return true;
 	}
 
-	private class TrieNode {
-		TrieNode[] children;
-		String word;
-		public TrieNode() {
-			children = new TrieNode[26];
-			word = null;
+	private void dfs(TrieNode cur, char[][] b, int row, int col, StringBuilder sb, List<String> res) {
+		char tmp = b[row][col];
+		if (cur == null || cur.children[tmp - 'a'] == null) return;
+		b[row][col] = '#';
+		sb.append(tmp);
+		if (cur.children[tmp - 'a'].isEnd) {
+			res.add(new String(sb));
+			cur.children[tmp - 'a'].isEnd = false;
 		}
+		for (int[] dir : dirs) {
+			int next_r = row + dir[0];
+			int next_c = col + dir[1];
+			if (!isValid(b, next_r, next_c)) continue;
+			dfs(cur.children[tmp - 'a'], b, next_r, next_c, sb, res);
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		b[row][col] = tmp;
 	}
 
-	private void buildTrie(TrieNode root, String word) {
+	private void buildTrie(String word) {
 		TrieNode cur = root;
 		for (int i = 0; i < word.length(); i++) {
-			char c = word.charAt(i);
-			if (cur.children[c - 'a'] == null) cur.children[c - 'a'] = new TrieNode();
-			cur = cur.children[c - 'a'];
+			int idx = word.charAt(i) - 'a';
+			if (cur.children[idx] == null) cur.children[idx] = new TrieNode();
+			cur = cur.children[idx];
 		}
-		cur.word = word;
+		cur.isEnd = true;
+	}
+
+	class TrieNode {
+		TrieNode[] children = new TrieNode[26];
+		boolean isEnd = false;
+		public TrieNode() {
+		}
 	}
 }
