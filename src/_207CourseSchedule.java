7c1177cdb20b2f0d789d9a9e671223b024b40296
fangzhouzhang <zhangark93@gmail.com>
2020-05-29 22:11:00 -0700

    202005292210

--- src/_207CourseSchedule.java
+++ src/_207CourseSchedule.java
@@ -1,33 +1,41 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class _207CourseSchedule {
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		if (numCourses == 0) return true;
+		if (prerequisites == null || prerequisites.length == 0) return true;
 		int[] indegree = new int[numCourses];
-		for (int[] prerequisite : prerequisites) {
-			indegree[prerequisite[0]]++;
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		for (int[] p : prerequisites) {
+			indegree[p[0]]++;
+			if (!map.containsKey(p[1])) {
+				map.put(p[1], new ArrayList<Integer>());
+			}
+			map.get(p[1]).add(p[0]);
 		}
+
 		Queue<Integer> q = new LinkedList<>();
-		for (int i = 0; i < indegree.length; i++) {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < numCourses; i++) {
 			if (indegree[i] == 0) {
 				q.add(i);
+				set.add(i);
 			}
 		}
 		while (!q.isEmpty()) {
-			int took = q.poll();
-			for (int[] prerequisite : prerequisites) {
-				if (took == prerequisite[1]) {
-					if (--indegree[prerequisite[0]] == 0) {
-						q.add(prerequisite[0]);
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				int cur = q.poll();
+				if (map.get(cur) == null) continue;
+				for (int nei : map.get(cur)) {
+					indegree[nei]--;
+					if (indegree[nei] == 0) {
+						q.add(nei);
+						set.add(nei);
 					}
 				}
 			}
 		}
-		for (int anIndegree : indegree) {
-			if (anIndegree != 0) {
-				return false;
-			}
-		}
-		return true;
+		return set.size() == numCourses;
 	}
 }
