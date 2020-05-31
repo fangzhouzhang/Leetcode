98172ceb8180811b46da121bc005bb8b3531b42e
fangzhouzhang <zhangark93@gmail.com>
2020-05-31 16:29:49 -0700

    202005311629

--- src/_1462CourseScheduleIV.java
+++ src/_1462CourseScheduleIV.java
@@ -1,4 +1,11 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class _1462CourseScheduleIV {
 	public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
