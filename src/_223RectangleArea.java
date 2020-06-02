631a9fd99a617f4cdf9551b5d0dd398b2dd7881d
fangzhouzhang <zhangark93@gmail.com>
2020-06-02 12:52:14 -0700

    202006021252

--- src/_223RectangleArea.java
+++ src/_223RectangleArea.java
@@ -1,10 +1,11 @@
 public class _223RectangleArea {
 	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-		int x1 = Math.max(A, E);
-		int y1 = Math.max(B, F);
-		int x2 = Math.min(C, G);
-		int y2 = Math.min(D, H);
-		if (x1 < x2 && y1 < y2) return Math.abs(C - A) * Math.abs(D - B) + Math.abs(E - G) * Math.abs(H - F) - Math.abs(x1 - x2) * Math.abs(y1 - y2);
-		return Math.abs(C - A) * Math.abs(D - B) + Math.abs(E - G) * Math.abs(H - F);
+		int x_left = Math.max(A, E);
+		int y_left = Math.max(B, F);
+		int x_right = Math.min(C, G);
+		int y_right = Math.min(D, H);
+
+		if (x_left <= x_right && y_left <= y_right) return (C - A) * (D - B) + (G - E) * (H - F) - (x_right - x_left) * (y_right - y_left);
+		return (C - A) * (D - B) + (G - E) * (H - F);
 	}
 }
