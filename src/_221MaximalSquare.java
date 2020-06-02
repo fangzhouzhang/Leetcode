f607011a840bd0d82e0f26d85fe332fba00ccb57
fangzhouzhang <zhangark93@gmail.com>
2020-06-02 09:20:34 -0700

    202006020920

--- src/_221MaximalSquare.java
+++ src/_221MaximalSquare.java
@@ -6,14 +6,27 @@ public class _221MaximalSquare {
      */
 	public int maximalSquare(char[][] matrix) {
 		if (matrix == null || matrix.length == 0) return 0;
-		int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
-		int len = 0;
-		for (int i = 1; i < dp.length; i++) {
-			for (int j = 1; j < dp[i].length; j++) {
-				if (matrix[i - 1][j - 1] == '1') dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1],dp[i - 1][j])) + 1;
-				len = Math.max(len, dp[i][j]);
+		int n = matrix.length;
+		int m = matrix[0].length;
+		int[][] dp = new int[n][m];
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			dp[i][0] = matrix[i][0] - '0';
+			res = Math.max(res, dp[i][0]);
+		}
+		for (int i = 0; i < m; i++) {
+			dp[0][i] = matrix[0][i] - '0';
+			res = Math.max(res, dp[0][i]);
+		}
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < m; j++) {
+				if (matrix[i][j] == '0') dp[i][j] = 0;
+				else {
+					dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+				}
+				res = Math.max(res, dp[i][j]);
 			}
 		}
-		return len * len;
+		return res * res;
 	}
 }
