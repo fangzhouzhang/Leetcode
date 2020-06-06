12bac0498e06e74e09bc59480cce9e0fc0514547
fangzhouzhang <zhangark93@gmail.com>
2020-06-06 00:37:43 -0700

    202006060037

--- src/_231PowerofTwo.java
+++ src/_231PowerofTwo.java
@@ -7,4 +7,14 @@ public class _231PowerofTwo {
 		if (n <= 0) return false;
 		return (n & (n - 1)) == 0;
 	}
+
+	public boolean isPowerOfTwo1(int n) {
+		if (n == 1) return true;
+		if (n <= 0 || (n % 2 == 1)) return false;
+		while (n > 1) {
+			if (n % 2 == 1) return false;
+			n >>= 1;
+		}
+		return true;
+	}
 }
