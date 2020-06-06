85b4d3d7853151af1759fc587202328a8ef038b4
fangzhouzhang <zhangark93@gmail.com>
2020-06-05 23:25:27 -0700

    202006052325

---
+++ src/_226InvertBinaryTree.java
@@ -0,0 +1,11 @@
+public class _226InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) return root;
+		invertTree(root.left);
+		invertTree(root.right);
+		TreeNode tmp = root.left;
+		root.left = root.right;
+		root.right = tmp;
+		return root;
+	}
+}
