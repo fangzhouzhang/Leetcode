126b1b31e2ca5475068e7cd9decd8ba401ed02d6
fangzhouzhang <zhangark93@gmail.com>
2020-06-11 00:22:05 -0700

    202006110021

--- src/_242ValidAnagram.java
+++ src/_242ValidAnagram.java
@@ -10,4 +10,24 @@ public class _242ValidAnagram {
 		}
 		return true;
 	}
+
+	public boolean isAnagramII(String s, String t) {
+		if (s == null && t == null) return true;
+		if (s == null || t == null) return false;
+		if (s.length() != t.length()) return false;
+		int[] map = new int[26];
+		int num = 0;
+		int n = s.length();
+		for (int i = 0; i < n; i++) {
+			int idx = s.charAt(i) - 'a';
+			map[idx]++;
+			if (map[idx] == 1) num++;
+		}
+		for (int i = 0; i < n; i++) {
+			int idx = t.charAt(i) - 'a';
+			map[idx]--;
+			if (map[idx] == 0) num--;
+		}
+		return num == 0;
+	}
 }
