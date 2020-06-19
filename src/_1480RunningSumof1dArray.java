8acc44a57ddad8970c1e3dad1c3a69b191fdb3a3
fangzhouzhang <zhangark93@gmail.com>
2020-06-18 23:43:47 -0700

    202006182343

---
+++ src/_1480RunningSumof1dArray.java
@@ -0,0 +1,10 @@
+public class _1480RunningSumof1dArray {
+	public int[] runningSum(int[] nums) {
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			nums[i] = sum;
+		}
+		return nums;
+	}
+}
