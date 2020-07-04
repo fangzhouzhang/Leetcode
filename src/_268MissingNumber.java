28c4a97bb350ee1ee700490a730495feebc3b09f
fangzhouzhang <zhangark93@gmail.com>
2020-07-03 23:54:55 -0700

    202007032354

--- src/_268MissingNumber.java
+++ src/_268MissingNumber.java
@@ -12,11 +12,17 @@ public class _268MissingNumber {
 	}
 
 	public int missingNumberXor(int[] nums) {
-		//https://www.youtube.com/watch?v=lBk6fVhuCyw
+		//https://www.youtube.com/watch?v=lBk6fVhuCyw0;
 		if (nums == null || nums.length == 0) return 0;
-		int res = nums[0];
-		for (int i = 1; i < nums.length; i++) res = res ^ nums[i];
-		for (int i = 0; i <= nums.length; i++) res = res ^ i;
-		return res;
+		int n = nums.length;
+		int val = nums[0];
+		for (int i = 1; i < n; i++) {
+			val ^= nums[i];
+		}
+		int v2 = 0;
+		for (int i = 1; i <= n; i++) {
+			v2 ^= i;
+		}
+		return val ^ v2;
 	}
 }
