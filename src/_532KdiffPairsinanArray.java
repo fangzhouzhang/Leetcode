21c542868a1e4c382ba95e06ac3e5b859e82b44c
fangzhouzhang <zhangark93@gmail.com>
2020-07-22 08:55:08 -0700

    202007220855

---
+++ src/_532KdiffPairsinanArray.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class _532KdiffPairsinanArray {
+	public int findPairs(int[] nums, int k) {
+		if (nums == null || nums.length == 0) return 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int n : nums) {
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+		int res = 0;
+		for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+			if (k == 0 && e.getValue() > 1) res++;
+			if (k > 0 && map.containsKey(e.getKey() + k)) res++;
+		}
+		return res;
+	}
+}
