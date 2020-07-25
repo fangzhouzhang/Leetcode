366c31501d665cad62208d4dcd56e8f3e83bed3e
fangzhouzhang <zhangark93@gmail.com>
2020-07-24 20:27:36 -0700

    202007242027

--- src/_443StringCompression.java
+++ src/_443StringCompression.java
@@ -13,4 +13,32 @@ public class _443StringCompression {
 		}
 		return cur;
 	}
+
+	public int compressII(char[] chars) {
+		if (chars == null || chars.length == 0) {
+			return 0;
+		}
+
+		String res = "";
+		int i = 0, j = 0, number = 0;
+		while (j < chars.length) {
+			if (chars[i] == chars[j]) {
+				j++;
+				number++;
+			} else {
+				res += chars[i];
+				res += number == 1 ? "" : number;
+				number = 0;
+				i = j;
+			}
+		}
+		if (number != 0) {
+			res += chars[i];
+			res += number == 1 ? "" : number;
+		}
+		for (int k = 0; k < res.length(); k++) {
+			chars[k] = res.charAt(k);
+		}
+		return res.length();
+	}
 }
