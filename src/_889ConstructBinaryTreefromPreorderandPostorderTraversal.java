6edcd2fc90967b88a8c90f83ea1325c73108efee
fangzhouzhang <zhangark93@gmail.com>
2020-07-29 21:48:19 -0700

    202007292148

--- src/_889ConstructBinaryTreefromPreorderandPostorderTraversal.java
+++ src/_889ConstructBinaryTreefromPreorderandPostorderTraversal.java
@@ -3,20 +3,23 @@ import java.util.Map;
 
 public class _889ConstructBinaryTreefromPreorderandPostorderTraversal {
 	public TreeNode constructFromPrePost(int[] pre, int[] post) {
-		if (pre == null || pre.length == 0 || post == null || post.length == 0) return null;
-		Map<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < post.length; i++) map.put(post[i], i);
-		return dfs(pre, 0, post, 0, pre.length, map);
+		if (pre == null || post == null) return null;
+		if (pre.length == 0 || post.length == 0) return null;
+		Map<Integer, Integer> p = new HashMap<>();
+		for (int i = 0; i < post.length; i++) {
+			p.put(post[i], i);
+		}
+		return dfs(pre, 0, post, 0, post.length, p);
 	}
 
-	private TreeNode dfs(int[] pre, int preIdx, int[] post, int postIdx, int toBeProcess, Map<Integer, Integer> map) {
-		if (toBeProcess == 0) return null;
-		TreeNode root = new TreeNode(pre[preIdx]);
-		if (toBeProcess == 1) return root;
-		int target = pre[preIdx + 1];
-		int leftSize = map.get(target) - postIdx + 1;
-		root.left = dfs(pre, preIdx + 1, post, postIdx, leftSize, map);
-		root.right = dfs(pre, preIdx + 1 + leftSize, post, postIdx + leftSize, toBeProcess - 1 - leftSize, map);
+	private TreeNode dfs(int[] pre, int i, int[] post, int j, int size, Map<Integer, Integer> p) {
+		if (size == 0) return null;
+		if (size == 1) return new TreeNode(pre[i]);
+		TreeNode root = new TreeNode(pre[i]);
+		int nextLeftNodeVal = pre[i + 1];
+		int leftSize = p.get(nextLeftNodeVal) - j + 1;
+		root.left = dfs(pre, i + 1, post, j, leftSize, p);
+		root.right = dfs(pre, i + 1 + leftSize, post, p.get(nextLeftNodeVal) + 1,  size - 1 - leftSize, p);
 		return root;
 	}
 }
