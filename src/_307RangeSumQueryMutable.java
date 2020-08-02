a1d39b7bf21860efbb0aba518387e225c2bb2bbc
fangzhouzhang <zhangark93@gmail.com>
2020-08-01 17:25:35 -0700

    202008011725

--- src/_307RangeSumQueryMutable.java
+++ src/_307RangeSumQueryMutable.java
@@ -1,31 +1,40 @@
 public class _307RangeSumQueryMutable {
 	class NumArray {
-		int[] trees;
-		int[] nums;
+		private int[] trees;
+		private int[] nums;
 		public NumArray(int[] nums) {
 			trees = new int[nums.length + 1];
-			this.nums = new int[nums.length];
-			// for (int i = 0; i < nums.length; i++) this.nums[i] = nums[i];
-			for (int i = 1; i < trees.length; i++) update(i - 1, nums[i - 1]);
+			this.nums = nums;
+			for (int i = 0; i < nums.length; i++) {
+				updateVal(i + 1, nums[i]);
+			}
+		}
+
+		private void updateVal(int idx, int delta) {
+			for (int i = idx; i < trees.length; i += lowbit(i)) {
+				trees[i] += delta;
+			}
 		}
 
 		public void update(int i, int val) {
-			int diff = val - nums[i];
-			for (int k = i + 1; k < trees.length; k += (k & -k)) trees[k] += diff;
+			updateVal(i + 1, val - nums[i]);
 			nums[i] = val;
 		}
 
 		public int sumRange(int i, int j) {
-			return getSum(j + 1) - getSum(i);
+			return query(j + 1) - query(i);
 		}
 
-		private int getSum(int i) {
+		private int query(int i) {
 			int sum = 0;
-			while (i > 0) {
-				sum += trees[i];
-				i -= (i & -i);
+			for (int k = i; k > 0; k -= lowbit(k)) {
+				sum += trees[k];
 			}
 			return sum;
+		}
+
+		private int lowbit(int x) {
+			return x & (-x);
 		}
 	}
 /**
