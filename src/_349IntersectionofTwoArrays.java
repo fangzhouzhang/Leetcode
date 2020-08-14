fcf32040b0b5bf6745ccf95828235ffe6ec1eb92
fangzhouzhang <zhangark93@gmail.com>
2020-08-13 22:46:36 -0700

    202008132246

--- src/_349IntersectionofTwoArrays.java
+++ src/_349IntersectionofTwoArrays.java
@@ -1,20 +1,31 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class _349IntersectionofTwoArrays {
 	public int[] intersection(int[] nums1, int[] nums2) {
-		Set<Integer> set = new HashSet<>();
-		Set<Integer> intersect = new HashSet<>();
-		for (int n : nums1) set.add(n);
-		for (int n : nums2) {
-			if (set.contains(n)) {
-				intersect.add(n);
-			}
+		if (nums1 == null || nums2 == null) return new int[0];
+		if (nums1.length < nums2.length) {
+			return intersection(nums2, nums1);
 		}
+		Set<Integer> set = new HashSet<>();
+		for (int n : nums2) {
+			set.add(n);
+		}
+		List<Integer> list = new ArrayList<>();
+		for (int n : nums1) {
+			if (set.contains(n)) {
+				list.add(n);
+				set.remove(n);
+			}
 
-		int[] res = new int[intersect.size()];
+		}
+		int[] res = new int[list.size()];
 		int idx = 0;
-		for (int n : intersect) res[idx++] = n;
+		for (int n : list) {
+			res[idx++] = n;
+		}
 		return res;
 	}
 }
