4d8dd61bed21583a452f36056ed1ac2ebf3c8151
fangzhouzhang <zhangark93@gmail.com>
2020-09-15 20:52:41 -0700

    202009152352

--- src/_465OptimalAccountBalancing.java
+++ src/_465OptimalAccountBalancing.java
@@ -43,4 +43,105 @@ public class _465OptimalAccountBalancing {
 		}
 		return min;
 	}
+
+	/**
+	 * 28/30 test cases
+	 * @param transactions
+	 * @return
+	 */
+	public int minTransfersWrong(int[][] transactions) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int[] t : transactions) {
+			if (!map.containsKey(t[0])) map.put(t[0], 0);
+			if (!map.containsKey(t[1])) map.put(t[1], 0);
+			map.put(t[0], map.get(t[0]) - t[2]);
+			map.put(t[1], map.get(t[1]) + t[2]);
+		}
+		Map<Integer, Integer> vals = new HashMap<>();
+		for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+			if (e.getValue() == 0) continue;
+			if (!vals.containsKey(e.getValue())) {
+				vals.put(e.getValue(), 1);
+			} else {
+				vals.put(e.getValue(), vals.get(e.getValue()) + 1);
+			}
+
+		}
+
+		int res = 0;
+		for (Map.Entry<Integer, Integer> e : vals.entrySet()) {
+			int curVal = e.getKey();
+			int oppoVal = -1 * curVal;
+			if (vals.containsKey(oppoVal) && (vals.get(oppoVal) > 0)) {
+				int curFreq = vals.get(curVal);
+				int oppoFreq = vals.get(oppoVal);
+				if (curFreq > oppoFreq) {
+					res += oppoFreq;
+					vals.put(curVal, curFreq - oppoFreq);
+					vals.put(oppoVal, 0);
+				} else if (curFreq < oppoFreq) {
+					res += curFreq;
+					vals.put(curVal, 0);
+					vals.put(oppoVal, oppoFreq - curFreq);
+				} else {
+					res += oppoFreq;
+					vals.put(curVal, 0);
+					vals.put(oppoVal, 0);
+				}
+			}
+		}
+		PriorityQueue<Pair> ns = new PriorityQueue<>(new MyComparator());
+		PriorityQueue<Pair> ps = new PriorityQueue<>(new MyComparator());
+		for (Map.Entry<Integer, Integer> e : vals.entrySet()) {
+			int num = e.getValue();
+			if (num == 0) continue;
+			for (int i = 0; i < num; i++) {
+				if (e.getKey() > 0) {
+					ps.add(new Pair(e.getKey(), e.getValue()));
+				}
+				if (e.getKey() < 0) {
+					ns.add(new Pair(Math.abs(e.getKey()), e.getValue()));
+				}
+			}
+		}
+
+		while (!ps.isEmpty() && !ns.isEmpty()) {
+			Pair p = ps.poll();
+			Pair n = ns.poll();
+			if (p.val > n.val) {
+				res++;
+				//put p back
+				p.val -= n.val;
+				ps.add(p);
+			} else if (p.val < n.val) {
+				//put p back
+				res++;
+				n.val -= p.val;
+				ns.add(n);
+			} else {
+				//just res++
+				res++;
+			}
+		}
+		return res;
+
+	}
+
+	class Pair {
+		int val;
+		int idx;
+		public Pair(int val, int idx) {
+			this.val = val;
+			this.idx = idx;
+		}
+	}
+
+	class MyComparator implements Comparator<Pair> {
+		public int compare(Pair a, Pair b) {
+			if (a.val != b.val) {
+				return b.val - a.val;
+			}
+			return 0;
+		}
+	}
 }
