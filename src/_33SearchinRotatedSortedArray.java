28591df74b2af3faefa37e7e9f8bcf52fc9f7c76
fangzhouzhang <zhangark93@gmail.com>
2020-10-14 15:34:01 -0700

    202010141833

--- src/_33SearchinRotatedSortedArray.java
+++ src/_33SearchinRotatedSortedArray.java
@@ -21,4 +21,33 @@ public class _33SearchinRotatedSortedArray {
 		}
 		return -1;
 	}
+
+	public int searchI(int[] nums, int target) {
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+		int start = 0, end = nums.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (nums[mid] == target) return mid;
+			if (nums[mid] >= nums[0]) {
+				//first part
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		if (nums[0] <= target && target <= nums[end]) return bs(nums, 0, end, target);
+		return bs(nums, end + 1, nums.length - 1, target);
+	}
+
+	private int bs(int[] a, int l, int r, int t) {
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+			if (a[mid] == t) return mid;
+			else if (a[mid] < t) l = mid + 1;
+			else r = mid - 1;
+		}
+		return -1;
+	}
 }
