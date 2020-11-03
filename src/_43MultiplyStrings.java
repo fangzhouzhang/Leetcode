e900ce5a115cd473db7070061dee169e01e973b3
fangzhouzhang <zhangark93@gmail.com>
2020-11-03 03:20:27 -0800

    202011030620

--- src/_43MultiplyStrings.java
+++ src/_43MultiplyStrings.java
@@ -1,27 +1,28 @@
 public class _43MultiplyStrings {
 	public String multiply(String num1, String num2) {
-		if (num1 == null || num2 == null) return "";
 		int n = num1.length(), m = num2.length();
-		int[] res = new int[m + n];
-		for (int i = n - 1; i >= 0; i--) {
-			for (int j = m - 1; j >= 0; j--) {
-				int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-				int curPos = i + j + 1;
-				int prePos = i + j;
-				int sum = product + res[curPos];
-				res[curPos] =  sum % 10;
-				res[prePos] += sum / 10;
+		int[] a = new int[n], b = new int[m];
+		int[] c = new int[n + m];
+		for (int i = n - 1, idx = 0; i >= 0; i--, idx++) {
+			a[idx] = num1.charAt(i) - '0';
+		}
+		for (int i = m - 1, idx = 0; i >= 0; i--, idx++) {
+			b[idx] = num2.charAt(i) - '0';
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				c[i + j] += a[i] * b[j];
 			}
 		}
+		for (int i = 0, t = 0; i < n + m; i++) {
+			t += c[i];
+			c[i] = t % 10;
+			t /= 10;
+		}
+		int k = n + m - 1;
+		while (k >= 0 && c[k] == 0) k--;
 		StringBuilder sb = new StringBuilder();
-		boolean allZero = true;
-		for (int i = 0; i < res.length; i++) {
-			if (res[i] == 0 && allZero) continue;
-			else {
-				allZero = false;
-				sb.append(res[i]);
-			}
-		}
+		while (k >= 0) sb.append((char)(c[k--] + '0'));
 		return sb.length() == 0 ? "0" : new String(sb);
 	}
 }
