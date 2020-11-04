b14d9562a5819186241237fb7e002202991ea97c
fangzhouzhang <zhangark93@gmail.com>
2020-11-04 06:18:13 -0800

    202011040918

--- src/_239SlidingWindowMaximum.java
+++ src/_239SlidingWindowMaximum.java
@@ -4,20 +4,15 @@ import java.util.Deque;
 public class _239SlidingWindowMaximum {
 	public int[] maxSlidingWindow(int[] nums, int k) {
 		if (nums == null || nums.length == 0) return new int[0];
-		Deque<Integer> dq = new ArrayDeque<>();
 		int n = nums.length;
+		Deque<Integer> dq = new ArrayDeque<>();
 		int[] res = new int[n - k + 1];
-		for (int i = 0; i < k; i++) {
-			while (!dq.isEmpty() && nums[dq.getLast()] < nums[i]) dq.pollLast();
+		int idx = 0;
+		for (int i = 0; i < n; i++) {
+			if (!dq.isEmpty() && dq.getFirst() < i - k + 1) dq.removeFirst();
+			while (!dq.isEmpty() && nums[dq.getLast()] <= nums[i]) dq.removeLast();
 			dq.addLast(i);
-		}
-		res[0] = nums[dq.getFirst()];
-		int idx = 1;
-		for (int i = k; i < n; i++) {
-			if (i - k >= dq.getFirst()) dq.pollFirst();
-			while (!dq.isEmpty() && nums[dq.getLast()] < nums[i]) dq.pollLast();
-			dq.addLast(i);
-			res[idx++] = nums[dq.getFirst()];
+			if (i >= k - 1) res[idx++] = nums[dq.getFirst()];
 		}
 		return res;
 	}
