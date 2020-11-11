489038fe23e9606876a2f6a02c00616822a1b66c
fangzhouzhang <zhangark93@gmail.com>
2020-11-10 19:57:19 -0800

    202011102257

---
+++ src/_94BinaryTreeInorderTraversal.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class _94BinaryTreeInorderTraversal {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		if (root == null) return res;
+		Stack<TreeNode> stack = new Stack<>();
+		while (root != null || !stack.isEmpty()) {
+			while (root != null) {
+				stack.push(root);
+				root = root.left;
+			}
+			if (!stack.isEmpty()) {
+				root = stack.pop();
+				res.add(root.val);
+				root = root.right;
+			}
+		}
+		return res;
+	}
+}
