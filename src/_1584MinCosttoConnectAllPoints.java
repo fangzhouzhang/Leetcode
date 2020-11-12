a25007485099322f6e3e4d31de44d7ebecf9ca20
fangzhouzhang <zhangark93@gmail.com>
2020-11-11 17:01:53 -0800

    202011112001

--- src/_1584MinCosttoConnectAllPoints.java
+++ src/_1584MinCosttoConnectAllPoints.java
@@ -1,51 +1,70 @@
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 
 public class _1584MinCosttoConnectAllPoints {
 	public int minCostConnectPoints(int[][] points) {
-		if (points == null || points.length <= 1) return 0;
-		//search all distance
-		Set<int[]> set = new HashSet<>();
-		int n = points.length;
-		set.add(points[0]);
 		int res = 0;
-
-		Queue<int[]> q = new LinkedList<>();
-		q.add(points[0]);
-		while (set.size() < n) {
-			int size = q.size();
-			PriorityQueue<Pair> pq = new PriorityQueue<>(new MyComparator());
-			for (int i = 0; i < size; i++) {
-				int[] cur = q.poll();
-				for (int[] next : points) {
-					if (set.contains(next)) continue;
-					pq.add(new Pair(Math.abs(cur[0] - next[0]) + Math.abs(cur[1] - next[1]), next));
-				}
+		int n = points.length;
+		UnionFind uf = new UnionFind(n);
+		int[][] g = new int[n * (n - 1) / 2][3];
+		int idx = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				int dis = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+				g[idx][0] = i;
+				g[idx][1] = j;
+				g[idx][2] = dis;
+				idx++;
 			}
-			Pair p = pq.poll();
-			res += p.val;
-			set.add(p.a);
-			for (int[] arr : set) q.add(arr);
 		}
-		return res;
+		Arrays.sort(g, new MyComparator());
+		for (int[] a: g) {
+			if (uf.union(a[0], a[1])) {
+				res += a[2];
+			}
+		}
+		return uf.n == 1 ? res : -1;
 	}
 
-	public class Pair {
-		int val;
-		int[] a;
-		public Pair(int val, int[] a) {
-			this.val = val;
-			this.a = a;
+	class MyComparator implements Comparator<int[]> {
+		public int compare(int[] a, int[] b) {
+			return a[2] - b[2];
 		}
 	}
 
-	public class MyComparator implements Comparator<Pair> {
-		public int compare(Pair a, Pair b) {
-			return a.val - b.val;
+	class UnionFind {
+		int[] parent;
+		int[] height;
+		int n;
+		public UnionFind(int size) {
+			parent = new int[size + 1];
+			for (int i = 1; i <= size; i++) {
+				parent[i] = i;
+			}
+			height = new int[size + 1];
+			Arrays.fill(height, 1);
+			this.n = size;
+		}
+
+		public int getParent(int x) {
+			if (parent[x] == x) return x;
+			return parent[x] = getParent(parent[x]);
+		}
+
+		public boolean union(int a, int b) {
+			int pa = getParent(a);
+			int pb = getParent(b);
+			if (pa == pb) return false;
+			if (height[pa] > height[pb]) {
+				parent[pb] = pa;
+			} else if (height[pa] < height[pb]) {
+				parent[pa] = pb;
+			} else {
+				parent[pa] = pb;
+				height[pb]++;
+			}
+			this.n--;
+			return true;
 		}
 	}
 }
