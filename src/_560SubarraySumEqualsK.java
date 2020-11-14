a6af8ec9b5782495d2a0d35127430bcde77ecf37
fangzhouzhang <zhangark93@gmail.com>
2020-11-13 21:23:53 -0800

    202011140023

--- src/_560SubarraySumEqualsK.java
+++ src/_560SubarraySumEqualsK.java
@@ -3,14 +3,15 @@ import java.util.Map;
 
 public class _560SubarraySumEqualsK {
 	public int subarraySum(int[] nums, int k) {
-		if (nums == null || nums.length == 0) return 0;
-		int idx = 0, sum = 0, res = 0;
 		Map<Integer, Integer> map = new HashMap<>();
-		while (idx < nums.length + 1) {
-			if (map.containsKey(sum - k)) res += map.get(sum - k);
+		int res = 0;
+		map.put(0, 1);
+		for (int i = 0, sum = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (map.containsKey(sum - k)) {
+				res += map.get(sum - k);
+			}
 			map.put(sum, map.getOrDefault(sum, 0) + 1);
-			if (idx < nums.length) sum += nums[idx];
-			idx++;
 		}
 		return res;
 	}
