882731a05a0cd5efa63b3c2bd284f96192609e9d
fangzhouzhang <zhangark93@gmail.com>
2020-11-16 18:44:33 -0800

    202011162144

--- src/_57InsertInterval.java
+++ src/_57InsertInterval.java
@@ -3,23 +3,35 @@ import java.util.List;
 
 public class _57InsertInterval {
 	public int[][] insert(int[][] intervals, int[] newInterval) {
-		if (intervals == null || intervals.length == 0 || newInterval == null || newInterval.length == 0) return new int[][]{newInterval};
-		List<int[]> res = new ArrayList<>();
-		for (int[] i : intervals) {
-			if (newInterval == null || i[1] < newInterval[0]) res.add(i);
-			else if (newInterval[1] < i[0]) {
-				res.add(newInterval);
-				newInterval = null;
-				res.add(i);
-			} else {
-				newInterval[0] = Math.min(i[0], newInterval[0]);
-				newInterval[1] = Math.max(i[1], newInterval[1]);
+		if (intervals == null || intervals.length == 0) {
+			int[][] res = {newInterval};
+			return res;
+		}
+		List<int[]> list = new ArrayList<>();
+		int k = 0, n = intervals.length;
+		while (k < n && intervals[k][1] < newInterval[0]) {
+			list.add(intervals[k++]);
+		}
+
+		if (k < n) {
+			while (k < n && intervals[k][0] <= newInterval[1]) {
+				newInterval[0] = Math.min(newInterval[0], intervals[k][0]);
+				newInterval[1] = Math.max(newInterval[1], intervals[k][1]);
+				k++;
 			}
 		}
-		if (newInterval != null) res.add(newInterval);
-		int[][] ans = new int[res.size()][2];
-		int i = 0;
-		for (int[] arr : res) ans[i++] = arr;
-		return ans;
+		list.add(newInterval);
+
+		while (k < n && intervals[k][0] > newInterval[1]) {
+			list.add(intervals[k++]);
+		}
+
+		n = list.size();
+		int[][] res = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			res[i][0] = list.get(i)[0];
+			res[i][1] = list.get(i)[1];
+		}
+		return res;
 	}
 }
