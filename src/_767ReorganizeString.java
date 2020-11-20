1424d330a8374b92f15043962fe09261d76cff9c
fangzhouzhang <zhangark93@gmail.com>
2020-11-20 09:20:29 -0800

    202011201220

--- src/_767ReorganizeString.java
+++ src/_767ReorganizeString.java
@@ -4,14 +4,17 @@ import java.util.PriorityQueue;
 public class _767ReorganizeString {
 	public String reorganizeString(String S) {
 		if (S == null || S.length() == 0) return "";
+		char[] a = S.toCharArray();
 		int[] map = new int[26];
-		for (char c : S.toCharArray()) map[c - 'a']++;
+		for (char c : a) {
+			int idx = (int)(c - 'a');
+			map[idx] += 1;
+		}
 		PriorityQueue<Pair> pq = new PriorityQueue<>(new MyComparator());
 		for (int i = 0; i < 26; i++) {
-			if (map[i] > 0) {
-				char c = (char)(i + 'a');
-				pq.add(new Pair(c, map[i]));
-			}
+			if (map[i] == 0) continue;
+			char c = (char)(i + 'a');
+			pq.add(new Pair(map[i], c));
 		}
 		StringBuilder sb = new StringBuilder();
 		while (!pq.isEmpty()) {
@@ -19,34 +22,33 @@ public class _767ReorganizeString {
 			if (sb.length() == 0 || sb.charAt(sb.length() - 1) != p.c) {
 				sb.append(p.c);
 				p.fre--;
-				if (p.fre > 0) pq.add(p);
-			} else if (sb.charAt(sb.length() - 1) == p.c) {
-				if (pq.size() == 0) return "";
-				Pair q = pq.poll();
-				sb.append(q.c);
-				q.fre--;
-				if (q.fre > 0) pq.add(q);
+				if (p.fre > 0) {
+					pq.add(p);
+				}
+			} else {
+				if (pq.isEmpty()) continue;
+				Pair another = pq.poll();
+				sb.append(another.c);
+				another.fre--;
+				if (another.fre > 0) pq.add(another);
 				pq.add(p);
 			}
-
 		}
-		return new String(sb);
+		return sb.length() == S.length() ? new String(sb) : "";
 	}
-
-	private class Pair{
-		char c;
-		int fre;
-		public Pair(char c, int fre) {
-			this.c = c;
-			this.fre = fre;
-		}
-	}
-
 
 	private class MyComparator implements Comparator<Pair> {
 		public int compare(Pair a, Pair b) {
-			if (a.fre != b.fre) return b.fre - a.fre;
-			else return a.c - b.c;
+			return b.fre - a.fre;
+		}
+	}
+
+	private class Pair {
+		int fre;
+		char c;
+		public Pair(int fre, char c) {
+			this.fre = fre;
+			this.c = c;
 		}
 	}
 }
