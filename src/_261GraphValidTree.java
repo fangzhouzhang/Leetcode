a82c4a82e88db2dacf58c14e8e922ef940e47fb3
fangzhouzhang <zhangark93@gmail.com>
2020-11-21 20:06:02 -0800

    202011212305

--- src/_261GraphValidTree.java
+++ src/_261GraphValidTree.java
@@ -1,31 +1,53 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _261GraphValidTree {
+	private int[] visit;
+	private int R;
+	private int C;
+	private List<Integer>[] g;
 	public boolean validTree(int n, int[][] edges) {
-		List<List<Integer>> adj = new ArrayList<>();
-		for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-		for (int[] e: edges) {
-			int u = e[0];
-			int v = e[1];
-			adj.get(u).add(v);
-			adj.get(v).add(u);
+		if (n - 1 != edges.length) return false;
+		visit = new int[n];
+		Arrays.fill(visit, -1);
+		g = new List[n];
+		for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+		for (int[] e : edges) {
+			g[e[0]].add(e[1]);
+			g[e[1]].add(e[0]);
 		}
-
-		boolean[] visit = new boolean[n];
-		if (dfs(adj, 0, -1, visit)) return false;
 		for (int i = 0; i < n; i++) {
-			if (!visit[i]) return false;
-		}
-		return true;
-	}
+			if (visit[i] == -1 && dfs(i, i)) {
+				boolean cc = true;
+				for (int j = 0; j < n; j++) {
+					if (visit[j] != 0) {
+						cc = false;
+						break;
+					}
+				}
+				if (cc) return true;
+				Arrays.fill(visit, -1);
+			}
 
-	private boolean dfs(List<List<Integer>> adj, int cur, int pre, boolean[] visit) {
-		visit[cur] = true;
-		for (int i = 0; i < adj.get(cur).size(); i++) {
-			int next = adj.get(cur).get(i);
-			if ((visit[next] && pre != next) || (!visit[next] && dfs(adj, next, cur, visit))) return true;
 		}
 		return false;
+	}
+
+	private boolean dfs(int v, int parent) {
+		visit[v] = 0;
+		for (int w : g[v]) {
+			if (visit[w] == -1) {
+				if (!dfs(w, v)) {
+					visit[v] = -1;
+					return false;
+				}
+			}
+			else if (w != parent) {
+				visit[v] = -1;
+				return false;
+			}
+		}
+		return true;
 	}
 }
