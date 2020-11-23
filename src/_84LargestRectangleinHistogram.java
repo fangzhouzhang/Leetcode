0c22d01979fa06bb92ea5d7c2ebde21f72182c3a
fangzhouzhang <zhangark93@gmail.com>
2020-11-22 20:22:09 -0800

    202011222322

--- src/_84LargestRectangleinHistogram.java
+++ src/_84LargestRectangleinHistogram.java
@@ -3,23 +3,28 @@ import java.util.Stack;
 public class _84LargestRectangleinHistogram {
 	public int largestRectangleArea(int[] heights) {
 		if (heights == null || heights.length == 0) return 0;
+		int n = heights.length;
+		int[] left = new int[n];
+		int[] right = new int[n];
 		Stack<Integer> stack = new Stack<>();
-		int max = -1;
-		int i = 0;
-		while (i < heights.length) {
-			if (stack.isEmpty() || heights[stack.peek()] <= heights[i]) {
-				stack.push(i++);
-			} else {
-				int h = heights[stack.pop()];
-				int w = stack.isEmpty() ? i : i - stack.peek() - 1;
-				max = Math.max(max, h * w);
-			}
+		for (int i = 0; i < n; i++) {
+			// strictly <, otherwise area will be less. example [1, 1]
+			while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+			if (stack.isEmpty()) left[i] = -1;
+			else left[i] = stack.peek();
+			stack.push(i);
 		}
-		while (!stack.isEmpty()) {
-			int h = heights[stack.pop()];
-			int w = stack.isEmpty() ? i : i - stack.peek() - 1;
-			max = Math.max(max, h * w);
+		stack.clear();
+		for (int i = n - 1; i >= 0; i--) {
+			while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) stack.pop();
+			if (stack.isEmpty()) right[i] = n;
+			else right[i] = stack.peek();
+			stack.push(i);
 		}
-		return max;
+		int res = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			res = Math.max(res, heights[i] * (right[i] - left[i] - 1));
+		}
+		return res;
 	}
 }
