328369ddfb9cf95ba20335e56669cc53299140f4
fangzhouzhang <zhangark93@gmail.com>
2020-11-27 10:15:13 -0800

    202011271315

--- src/_1302DeepestLeavesSum.java
+++ src/_1302DeepestLeavesSum.java
@@ -3,24 +3,20 @@ import java.util.*;
 public class _1302DeepestLeavesSum {
 	public int deepestLeavesSum(TreeNode root) {
 		if (root == null) return 0;
-		Map<Integer, List<Integer>> map = new HashMap<>();
-		int res = 0;
 		Queue<TreeNode> q = new LinkedList<>();
 		q.add(root);
-		int level = 0;
+		int res = 0;
 		while (!q.isEmpty()) {
 			int size = q.size();
-			for (int i = 0; i < size; i++) {
-				TreeNode node = q.poll();
-				if (!map.containsKey(level)) map.put(level, new ArrayList<Integer>());
-				map.get(level).add(node.val);
-				if (node.left != null) q.add(node.left);
-				if (node.right != null) q.add(node.right);
+			int tmp = 0;
+			for (int k = 0; k < size; k++) {
+				TreeNode cur = q.poll();
+				tmp += cur.val;
+				if (cur.left != null) q.add(cur.left);
+				if (cur.right != null) q.add(cur.right);
 			}
-			level++;
+			res = tmp;
 		}
-		List<Integer> list = map.get(level - 1);
-		for (int n : list) res += n;
 		return res;
 	}
 }
