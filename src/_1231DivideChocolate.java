9cbf655751921d04db27f6274cb7646d395ff9ed
fangzhouzhang <zhangark93@gmail.com>
2020-11-27 09:38:57 -0800

    202011271238

--- src/_1231DivideChocolate.java
+++ src/_1231DivideChocolate.java
@@ -1,35 +1,28 @@
 public class _1231DivideChocolate {
 	public int maximizeSweetness(int[] sweetness, int K) {
-		if (null == sweetness) return 0;
-		int n = sweetness.length;
-		if (0 == n) return 0;
-		int sum = 0;
-		for (int i = 1; i <= n; i++) {
-			sum += sweetness[i - 1];
+		int min = Integer.MAX_VALUE, sum = 0;
+		for (int s : sweetness) {
+			min = Math.min(min, s);
+			sum += s;
 		}
-		int end = sum / (K + 1);
-		int start = 0;
+		int start = min, end = sum;
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
-			if (check(sweetness, K + 1, mid)) {
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
+			if (valid(sweetness, mid, K + 1)) start = mid + 1;
+			else end = mid - 1;
 		}
 		return end;
 	}
 
-	private boolean check(int[] a, int k, int sum) {
-		int s = 0;
-		for (int i = 0; i < a.length; i++ ) {
-			s += a[i];
-			if (s >= sum) {
-				s = 0;
-				k--;
-				if (k == 0) break;
+	private boolean valid(int[] sweetness, int sum, int m) {
+		int cur = 0, cnt = 0;
+		for (int s : sweetness) {
+			cur += s;
+			if (cur >= sum) {
+				cnt++;
+				cur = 0;
 			}
 		}
-		return k == 0;
+		return cnt >= m;
 	}
 }
