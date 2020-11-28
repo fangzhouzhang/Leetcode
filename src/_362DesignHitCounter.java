b7bc6e93665795ffb9025a3a287a6a2316c0ef01
fangzhouzhang <zhangark93@gmail.com>
2020-11-28 08:26:32 -0800

    202011281126

--- src/_362DesignHitCounter.java
+++ src/_362DesignHitCounter.java
@@ -1,25 +1,35 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class _362DesignHitCounter {
 	private class HitCounter {
-		private Queue<Integer> q;
+		private int[] hits;
+		private int[] ts;
+
 		/** Initialize your data structure here. */
 		public HitCounter() {
-			q = new LinkedList<>();
+			hits = new int[300];
+			ts = new int[300];
 		}
 
 		/** Record a hit.
 		 @param timestamp - The current timestamp (in seconds granularity). */
 		public void hit(int timestamp) {
-			q.add(timestamp);
+			int idx = timestamp % 300;
+			if (ts[idx] != timestamp) {
+				hits[idx] = 1;
+				ts[idx] = timestamp;
+			} else {
+				hits[idx]++;
+			}
 		}
 
 		/** Return the number of hits in the past 5 minutes.
 		 @param timestamp - The current timestamp (in seconds granularity). */
 		public int getHits(int timestamp) {
-			while (!q.isEmpty() && timestamp - q.peek() >= 300) q.poll();
-			return q.size();
+			int res = 0;
+			for (int i = 0; i < 300; i++) {
+				if (timestamp - ts[i] < 300) res += hits[i];
+			}
+			return res;
 		}
 	}
+
 }
