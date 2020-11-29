ee9cd7e972f2125e3b779ea7c16fbfa9fda1e8cd
fangzhouzhang <zhangark93@gmail.com>
2020-11-28 19:52:42 -0800

    202011282252

--- src/_739DailyTemperatures.java
+++ src/_739DailyTemperatures.java
@@ -2,16 +2,18 @@ import java.util.Stack;
 
 public class _739DailyTemperatures {
 	public int[] dailyTemperatures(int[] T) {
-		if (T == null || T.length == 0) return T;
-		int[] res = new int[T.length];
+		if (T == null || T.length == 0) return new int[0];
+		int n = T.length;
 		Stack<Integer> stack = new Stack<>();
-		int idx = 0;
-		while (idx < T.length) {
-			if (stack.isEmpty() || T[stack.peek()] >= T[idx]) stack.push(idx++);
-			else {
-				int largerTempIdx = stack.pop();
-				res[largerTempIdx] = idx - largerTempIdx;
+		int[] res = new int[n];
+		for (int i = n - 1, j = n - 1; i >= 0; i--, j--) {
+			while (!stack.isEmpty() && T[stack.peek()] <= T[i]) stack.pop();
+			if (stack.isEmpty()) {
+				res[j] = 0;
+			} else {
+				res[j] = stack.peek() - i;
 			}
+			stack.push(i);
 		}
 		return res;
 	}
