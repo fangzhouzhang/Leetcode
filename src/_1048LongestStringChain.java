899ea676c74cdfded39293c51d2254ae3b7c8805
fangzhouzhang <zhangark93@gmail.com>
2020-11-30 09:20:10 -0800

    202011301220

--- src/_1048LongestStringChain.java
+++ src/_1048LongestStringChain.java
@@ -1,40 +1,23 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _1048LongestStringChain {
 	public int longestStrChain(String[] words) {
-		if (words == null || words.length == 0) return 0;
 		Arrays.sort(words, new MyComparator());
-		Integer[] dp = new Integer[words.length];
-		for (int i = 0; i < words.length; i++) dfs(words, i, dp);
+		Map<String, Integer> map = new HashMap<>();
 		int res = 1;
-		for (int n : dp) res = Math.max(res, n);
-		return res;
-	}
-
-	private int dfs(String[] words, int idx, Integer[] dp) {
-		// if (idx == words.length) return 0;
-		if (dp[idx] != null) return dp[idx];
-		int max = 1;
-		for (int i = idx + 1; i < words.length; i++) {
-			if (isPredecessor(words[idx].toCharArray(), words[i].toCharArray())) {
-				max = Math.max(max, dfs(words, i, dp) + 1);
+		for (String word : words) {
+			int tmp = 1;
+			for (int i = 0; i < word.length(); i++) {
+				String cur = word.substring(0, i) + word.substring(i + 1);
+				tmp = Math.max(tmp, map.getOrDefault(cur, 0) + 1);
 			}
+			res = Math.max(res, tmp);
+			map.put(word, tmp);
 		}
-		dp[idx] = max;
-		return max;
-	}
-
-	private boolean isPredecessor(char[] a, char[] b) {
-		if (a.length + 1 != b.length) return false;
-		int i = 0, j = 0;
-		while (i < a.length && j < b.length) {
-			if (a[i] == b[j]) {
-				i++;
-				j++;
-			} else j++;
-		}
-		return i == a.length;
+		return res;
 	}
 
 	private class MyComparator implements Comparator<String> {
