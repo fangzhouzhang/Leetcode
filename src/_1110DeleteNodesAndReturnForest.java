b8e611692d23eaec8104e7126b24e1bbbeefb41d
fangzhouzhang <zhangark93@gmail.com>
2020-11-30 08:40:18 -0800

    202011301140

--- src/_1110DeleteNodesAndReturnForest.java
+++ src/_1110DeleteNodesAndReturnForest.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class _1110DeleteNodesAndReturnForest {
 	public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+		if (root == null) return new ArrayList<>();
 		List<TreeNode> res = new ArrayList<>();
-		if (root == null) return res;
 		Set<Integer> set = new HashSet<>();
-		for (int n : to_delete) set.add(n);
-		dfs(root, null, set, res);
+		for (int del : to_delete) set.add(del);
+		dfs(root, set, res, true);
 		return res;
 	}
 
-	private void dfs(TreeNode root, TreeNode parent, Set<Integer> set, List<TreeNode> res) {
-		if (root == null) return;
-		dfs(root.left, root, set, res);
-		if (root.left != null && set.contains(root.left.val)) root.left = null;
-		dfs(root.right, root, set, res);
-		if (root.right != null && set.contains(root.right.val)) root.right = null;
-		if ((!set.contains(root.val)) && (parent == null || set.contains(parent.val))) res.add(root);
+	private TreeNode dfs(TreeNode root, Set<Integer> set, List<TreeNode> res, boolean father) {
+		if (root == null) return null;
+		boolean nextFather = set.contains(root.val);
+		root.left = dfs(root.left, set, res, nextFather);
+		root.right = dfs(root.right, set, res, nextFather);
+		if (father && !nextFather) res.add(root);
+		return nextFather ? null : root;
 	}
 }
