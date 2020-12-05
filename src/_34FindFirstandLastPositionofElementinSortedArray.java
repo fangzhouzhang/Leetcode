9a16fbaee0a7880c6f3d8fccd00b9569913f1861
fangzhouzhang <zhangark93@gmail.com>
2020-12-04 20:44:01 -0800

    202012042343

--- src/_34FindFirstandLastPositionofElementinSortedArray.java
+++ src/_34FindFirstandLastPositionofElementinSortedArray.java
@@ -32,4 +32,26 @@ public class _34FindFirstandLastPositionofElementinSortedArray {
 		if (nums[start] == target) return start;
 		return -1;
 	}
+
+	public int[] searchRangeI(int[] nums, int target) {
+		if (nums == null || nums.length == 0) return new int[]{-1, -1};
+		int n = nums.length;
+		int l = 0, r = n - 1;
+		while (l < r) {
+			int mid = l + r >> 1;
+			if (nums[mid] < target) l = mid + 1;
+			else r = mid;
+		}
+		int[] res = {-1, -1};
+		if (nums[l] != target) return new int[]{-1, -1};
+		res[0] = l;
+		r = n - 1;
+		while (l < r) {
+			int mid = l + r + 1>> 1;
+			if (nums[mid] > target) r = mid - 1;
+			else l = mid;
+		}
+		res[1] = r;
+		return res;
+	}
 }
