491397aebe39063eb193e61440715d97db3c94bd
fangzhouzhang <zhangark93@gmail.com>
2020-12-25 20:37:03 -0800

    202012252336

--- src/_1696JumpGameVI.java
+++ src/_1696JumpGameVI.java
@@ -1,19 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class _1696JumpGameVI {
-	int k = 0;
 	public int maxResult(int[] nums, int k) {
 		if (nums == null || nums.length == 0) return 0;
-		this.k = k;
-		Integer[] dp = new Integer[nums.length];
-		return dfs(nums, 0, dp);
-	}
-
-	private int dfs(int[] nums, int idx, Integer[] dp) {
-		if (idx == nums.length - 1) return nums[idx];
-		if (dp[idx] != null) return dp[idx];
-		int res = Integer.MIN_VALUE;
-		for (int i = 1; i <= k && i + idx <= nums.length - 1; i++) {
-			res = Math.max(res, dfs(nums, i + idx, dp) + nums[idx]);
+		int n = nums.length;
+		int[] dp = new int[n];
+		Deque<Integer> dq = new ArrayDeque<>();
+		dp[0] = nums[0];
+		dq.add(0);
+		for (int i = 1; i < n; i++) {
+			while (!dq.isEmpty() && i - dq.peekFirst() > k) dq.pollFirst();
+			dp[i] = dq.isEmpty() ? nums[i] : dp[dq.peekFirst()] + nums[i];
+			while (!dq.isEmpty() && dp[i] >= dp[dq.peekLast()]) dq.pollLast();
+			dq.addLast(i);
 		}
-		return dp[idx] = res;
+		return dp[n - 1];
 	}
 }
