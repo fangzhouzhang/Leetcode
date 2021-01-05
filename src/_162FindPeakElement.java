90f1a9bf3e3d7456afd4a355f0c9c19b2c3f8d2e
fangzhouzhang <zhangark93@gmail.com>
2021-01-04 19:59:50 -0800

    202101042300

--- src/_162FindPeakElement.java
+++ src/_162FindPeakElement.java
@@ -1,18 +1,12 @@
 public class _162FindPeakElement {
 	public int findPeakElement(int[] nums) {
 		if (null == nums || nums.length == 0) return -1;
-		int start = 0, end = nums.length - 1;
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			long left = (mid - 1 < 0 || mid - 1 >= nums.length) ? Long.MIN_VALUE : (long)nums[mid - 1];
-			long right = (mid + 1 < 0 || mid + 1 >= nums.length) ? Long.MIN_VALUE : (long)nums[mid + 1];
-			if (nums[mid] > left && nums[mid] > right) return mid;
-			else if (nums[mid] < left && nums[mid] > right) {
-				end = mid - 1;
-			} else if (nums[mid] > left && nums[mid] < right) {
-				start = mid + 1;
-			} else start++;
+		int l = 0, r = nums.length - 1;
+		while (l < r) {
+			int mid = l + r + 1 >> 1;
+			if (nums[mid - 1] < nums[mid]) l = mid;
+			else r = mid - 1;
 		}
-		return -1;
+		return l;
 	}
 }
