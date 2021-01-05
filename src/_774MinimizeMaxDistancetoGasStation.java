e86dd4f58638a623fec76d513be4801678ba39f9
fangzhouzhang <zhangark93@gmail.com>
2021-01-05 08:53:54 -0800

    202101051153

--- src/_774MinimizeMaxDistancetoGasStation.java
+++ src/_774MinimizeMaxDistancetoGasStation.java
@@ -1,26 +1,23 @@
 public class _774MinimizeMaxDistancetoGasStation {
+	private static final double ERROR = 1e-6;
 	public double minmaxGasDist(int[] stations, int K) {
 		if (stations == null || stations.length == 0) return 0;
-		double start = 0;
-		double end = stations[stations.length - 1] - stations[0];
-		while (end - start > 1e-6) {
-			double mid = start + (end - start) / 2;
-			if (isValid(mid, stations, K)) {
-				end = mid;
-			} else {
-				start = mid;
-			}
+		int n = stations.length;
+		double l = 0, r = stations[n - 1] - stations[0];
+		while (l + ERROR <= r) {
+			double mid = (r + l) / 2;
+			if (isOk(mid, stations, K)) r = mid;
+			else l = mid + ERROR;
 		}
-		return start;
+		return r;
 	}
 
-	private boolean isValid(double mid, int[] stations, int K) {
-		int sum = 0, n = stations.length;
-		for (int i = 1; i < n; i++) {
-			double dist = stations[i] - stations[i - 1];
-			sum += Math.ceil(dist / mid) - 1;
-			if (sum > K) return false;
+	private boolean isOk(double mid, int[] s, int k) {
+		int count = 0;
+		for (int i = 1; i < s.length; i++) {
+			int cur = s[i] - s[i - 1];
+			count += Math.ceil(cur * 1.0 / mid) - 1;
 		}
-		return sum <= K;
+		return count <= k;
 	}
 }
