0d099dc2edf8f44ac13271b59f31c17c5eac13c7
fangzhouzhang <zhangark93@gmail.com>
2021-01-05 08:39:02 -0800

    202101051138

--- src/_287FindtheDuplicateNumber.java
+++ src/_287FindtheDuplicateNumber.java
@@ -1,18 +1,45 @@
 public class _287FindtheDuplicateNumber {
+	/**
+	 * version 1: [l, mid - 1], [mid, r]
+	 * @param nums
+	 * @return
+	 */
 	public int findDuplicate(int[] nums) {
 		if (nums == null || nums.length == 0) return 0;
-		int start = 1, end = nums.length;
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-			int small = 0, equal = 0;
-			for (int n : nums) {
-				if (n < mid) small++;
-				if (n == mid) equal++;
+		int n = nums.length;
+		int l = 1, r = n;
+		while (l < r) {
+			int mid = l + r + 1 >> 1;
+			int count = 0;
+			for (int x : nums) {
+				if (x >= l && x < mid) count++;
 			}
-			if (equal > 1) return mid;
-			if (small > mid - 1) end = mid - 1;
-			else start = mid + 1;
+
+			if (count > mid - l) r = mid - 1;
+			else l = mid;
 		}
-		return -1;
+		return l;
+	}
+
+	/**
+	 * version 2: [l, mid], [mid + 1, r]
+	 * @param nums
+	 * @return
+	 */
+	public int findDuplicateI(int[] nums) {
+		if (nums == null || nums.length == 0) return 0;
+		int n = nums.length;
+		int l = 1, r = n;
+		while (l < r) {
+			int mid = l + r >> 1;
+			int count = 0;
+			for (int x : nums) {
+				if (x >= l && x <= mid) count++;
+			}
+
+			if (count > mid - l + 1) r = mid;
+			else l = mid + 1;
+		}
+		return l;
 	}
 }
