80d8fb901d8535692a84fa34670e7d93987e291b
fangzhouzhang <zhangark93@gmail.com>
2021-01-23 21:57:24 -0800

    202101232157

---
+++ src/_1732FindtheHighestAltitude.java
@@ -0,0 +1,11 @@
+public class _1732FindtheHighestAltitude {
+	public int largestAltitude(int[] gain) {
+		int max = 0;
+		int h = 0;
+		for (int i = 0; i < gain.length; i++) {
+			h += gain[i];
+			max = Math.max(max, h);
+		}
+		return max;
+	}
+}
