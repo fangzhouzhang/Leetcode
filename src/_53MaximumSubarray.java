4de85739d1a552e105272e2b40d17414b691122e
fangzhouzhang <zhangark93@gmail.com>
2021-02-05 07:26:53 -0800

    202102050726

--- src/_53MaximumSubarray.java
+++ src/_53MaximumSubarray.java
@@ -1,12 +1,11 @@
 public class _53MaximumSubarray {
 	public int maxSubArray(int[] nums) {
-		if (nums.length == 0) return 0;
-		int prev = nums[0];
+		if (nums == null || nums.length == 0) return 0;
+		int n = nums.length;
 		int max = nums[0];
-		for (int i = 1; i < nums.length; i++) {
-			if (prev < 0) prev = nums[i];
-			else prev += nums[i];
-			max = Math.max(max, prev);
+		for (int i = 1; i < n; i++) {
+			nums[i] = Math.max(nums[i], nums[i - 1] + nums[i]);
+			max = Math.max(max, nums[i]);
 		}
 		return max;
 	}
