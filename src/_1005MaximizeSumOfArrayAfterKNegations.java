0d13c53e1f353caecdd8c34d13a7d3c65042b4e7
fangzhouzhang <zhangark93@gmail.com>
2021-04-29 23:06:45 -0700

    202104292306

---
+++ src/_1005MaximizeSumOfArrayAfterKNegations.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+public class _1005MaximizeSumOfArrayAfterKNegations {
+	public int largestSumAfterKNegations(int[] A, int K) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+		for(int x: A) pq.add(x);
+		while( K--  > 0) pq.add(-pq.poll());
+
+		int sum  = 0;
+		for(int i = 0; i < A.length; i++){
+			sum += pq.poll();
+		}
+		return sum;
+	}
+}
